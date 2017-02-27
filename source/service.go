18318b84bed3d487907e79f140d45f0400ed56e4
Justin Santa Barbara <justin@fathomdb.com>
2017-02-26 22:21:38 -0500

    Extract Status.LoadBalancer.Hostname from service annotations
    On AWS, the loadbalancer is / will be / should be set up in the
    hostname.

--- source/service.go
+++ source/service.go
@@ -43,7 +43,7 @@ type ServiceSource struct {
 
 // Endpoints returns endpoint objects for each service that should be processed.
 func (sc *ServiceSource) Endpoints() ([]endpoint.Endpoint, error) {
-	services, err := sc.Client.Core().Services(v1.NamespaceAll).List(v1.ListOptions{})
+	services, err := sc.Client.CoreV1().Services(v1.NamespaceAll).List(v1.ListOptions{})
 	if err != nil {
 		return nil, err
 	}
@@ -51,29 +51,48 @@ func (sc *ServiceSource) Endpoints() ([]endpoint.Endpoint, error) {
 	endpoints := []endpoint.Endpoint{}
 
 	for _, svc := range services.Items {
-		// Check controller annotation to see if we are responsible.
-		controller, exists := svc.Annotations[controllerAnnotationKey]
-		if exists && controller != controllerAnnotationValue {
-			continue
-		}
-
-		// Get the desired hostname of the service from the annotation.
-		hostname, exists := svc.Annotations[hostnameAnnotationKey]
-		if !exists {
-			continue
-		}
-
-		// Create an endpoint matching the desired hostname.
-		endpoint := endpoint.Endpoint{
-			DNSName: hostname,
-		}
-
-		// Create a corresponding endpoint for each configured external entrypoint.
-		for _, lb := range svc.Status.LoadBalancer.Ingress {
-			endpoint.Target = lb.IP
-			endpoints = append(endpoints, endpoint)
+		svcEndpoints := endpointsFromService(&svc)
+		if len(svcEndpoints) != 0 {
+			endpoints = append(endpoints, svcEndpoints...)
 		}
 	}
 
 	return endpoints, nil
+}
+
+// endpointsFromService extracts the endpoints from a service object
+func endpointsFromService(svc *v1.Service) []endpoint.Endpoint {
+	var endpoints []endpoint.Endpoint
+
+	// Check controller annotation to see if we are responsible.
+	controller, exists := svc.Annotations[controllerAnnotationKey]
+	if exists && controller != controllerAnnotationValue {
+		return endpoints
+	}
+
+	// Get the desired hostname of the service from the annotation.
+	hostname, exists := svc.Annotations[hostnameAnnotationKey]
+	if !exists {
+		return endpoints
+	}
+
+	// Create a corresponding endpoint for each configured external entrypoint.
+	for _, lb := range svc.Status.LoadBalancer.Ingress {
+		if lb.IP != "" {
+			endpoint := endpoint.Endpoint{
+				DNSName: hostname,
+				Target:  lb.IP,
+			}
+			endpoints = append(endpoints, endpoint)
+		}
+		if lb.Hostname != "" {
+			endpoint := endpoint.Endpoint{
+				DNSName: hostname,
+				Target:  lb.Hostname,
+			}
+			endpoints = append(endpoints, endpoint)
+		}
+	}
+
+	return endpoints
 }
