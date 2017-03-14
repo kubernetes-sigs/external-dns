634f1063e9d9251207fdf4290b43a5d1de4d0e4f
ideahitme <yerken.tussupbekov@zalando.de>
2017-03-14 14:04:21 +0100

    add testing to storage

--- storage/inmemory_test.go
+++ storage/inmemory_test.go
@@ -15,3 +15,43 @@ limitations under the License.
 */
 
 package storage
+
+import (
+	"testing"
+
+	"github.com/kubernetes-incubator/external-dns/dnsprovider"
+)
+
+func TestInMemory(t *testing.T) {
+	t.Run("TestNewInMemoryStorage", testNewInMemoryStorage)
+}
+
+func testNewInMemoryStorage(t *testing.T) {
+	var owner, zone string
+	var provider dnsprovider.InMemoryProvider
+	if _, err := NewInMemoryStorage(&provider, owner, zone); err == nil {
+		t.Errorf("should fail when owner/zone is empty")
+	}
+	owner = "test-1"
+	zone = ""
+	if _, err := NewInMemoryStorage(&provider, owner, zone); err == nil {
+		t.Errorf("should fail when owner/zone is empty")
+	}
+	owner = ""
+	zone = "zone-1"
+	if _, err := NewInMemoryStorage(&provider, owner, zone); err == nil {
+		t.Errorf("should fail when owner/zone is empty")
+	}
+	owner = "test-1"
+	zone = "zone-1"
+	storage, err := NewInMemoryStorage(&provider, owner, zone)
+	if err != nil {
+		t.Error(err)
+	}
+	if storage.zone != zone || storage.owner != owner || storage.cache == nil {
+		t.Errorf("incorrectly initialized in memory storage provider")
+	}
+	if _, ok := storage.registry.(*dnsprovider.InMemoryProvider); !ok {
+		t.Errorf("incorrect dns provider is used for registry")
+	}
+}
