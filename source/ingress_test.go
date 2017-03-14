08d752c592e231250ccaeb8a9701cb9ad305b6a0
Martin Linkhorst <linki@users.noreply.github.com>
2017-03-14 10:54:24 +0100

    Ingress: appends trailing dot to hostnames (#85)
    * fix(ingress): append trailing dot to hostnames
    
    * fix(source): update tests for sanitized DNS names

--- source/ingress_test.go
+++ source/ingress_test.go
@@ -47,7 +47,7 @@ func testEndpointsFromIngress(t *testing.T) {
 			},
 			expected: []endpoint.Endpoint{
 				{
-					DNSName: "foo.bar",
+					DNSName: "foo.bar.",
 					Target:  "lb.com",
 				},
 			},
@@ -60,7 +60,7 @@ func testEndpointsFromIngress(t *testing.T) {
 			},
 			expected: []endpoint.Endpoint{
 				{
-					DNSName: "foo.bar",
+					DNSName: "foo.bar.",
 					Target:  "8.8.8.8",
 				},
 			},
@@ -74,19 +74,19 @@ func testEndpointsFromIngress(t *testing.T) {
 			},
 			expected: []endpoint.Endpoint{
 				{
-					DNSName: "foo.bar",
+					DNSName: "foo.bar.",
 					Target:  "8.8.8.8",
 				},
 				{
-					DNSName: "foo.bar",
+					DNSName: "foo.bar.",
 					Target:  "127.0.0.1",
 				},
 				{
-					DNSName: "foo.bar",
+					DNSName: "foo.bar.",
 					Target:  "elb.com",
 				},
 				{
-					DNSName: "foo.bar",
+					DNSName: "foo.bar.",
 					Target:  "alb.com",
 				},
 			},
@@ -154,11 +154,11 @@ func testIngressEndpoints(t *testing.T) {
 			},
 			expected: []endpoint.Endpoint{
 				{
-					DNSName: "example.org",
+					DNSName: "example.org.",
 					Target:  "8.8.8.8",
 				},
 				{
-					DNSName: "new.org",
+					DNSName: "new.org.",
 					Target:  "lb.com",
 				},
 			},
@@ -182,11 +182,11 @@ func testIngressEndpoints(t *testing.T) {
 			},
 			expected: []endpoint.Endpoint{
 				{
-					DNSName: "example.org",
+					DNSName: "example.org.",
 					Target:  "8.8.8.8",
 				},
 				{
-					DNSName: "new.org",
+					DNSName: "new.org.",
 					Target:  "lb.com",
 				},
 			},
@@ -210,7 +210,7 @@ func testIngressEndpoints(t *testing.T) {
 			},
 			expected: []endpoint.Endpoint{
 				{
-					DNSName: "example.org",
+					DNSName: "example.org.",
 					Target:  "8.8.8.8",
 				},
 			},
