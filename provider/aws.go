433e6b1a6d599c6402c4e1bc852bb84bf4c0d7f4
Martin Linkhorst <martin.linkhorst+github.com@posteo.de>
2017-04-05 18:15:22 +0200

    fix(aws): improve quality of test code

--- provider/aws.go
+++ provider/aws.go
@@ -17,10 +17,7 @@ limitations under the License.
 package provider
 
 import (
-	"fmt"
-
 	log "github.com/Sirupsen/logrus"
-	"github.com/google/uuid"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -35,10 +32,7 @@ import (
 type Route53API interface {
 	ListResourceRecordSetsPages(input *route53.ListResourceRecordSetsInput, fn func(resp *route53.ListResourceRecordSetsOutput, lastPage bool) (shouldContinue bool)) error
 	ChangeResourceRecordSets(*route53.ChangeResourceRecordSetsInput) (*route53.ChangeResourceRecordSetsOutput, error)
-	ListHostedZonesPages(input *route53.ListHostedZonesInput, fn func(resp *route53.ListHostedZonesOutput, lastPage bool) (shouldContinue bool)) error
-	ListHostedZonesByName(input *route53.ListHostedZonesByNameInput) (*route53.ListHostedZonesByNameOutput, error)
 	CreateHostedZone(*route53.CreateHostedZoneInput) (*route53.CreateHostedZoneOutput, error)
-	DeleteHostedZone(*route53.DeleteHostedZoneInput) (*route53.DeleteHostedZoneOutput, error)
 }
 
 // AWSProvider is an implementation of Provider for AWS Route53.
@@ -67,84 +61,8 @@ func NewAWSProvider(dryRun bool) (Provider, error) {
 	return provider, nil
 }
 
-// Zones returns the list of hosted zones.
-func (p *AWSProvider) Zones() ([]string, error) {
-	zones := []string{}
-
-	f := func(resp *route53.ListHostedZonesOutput, lastPage bool) (shouldContinue bool) {
-		for _, zone := range resp.HostedZones {
-			zones = append(zones, aws.StringValue(zone.Name))
-		}
-
-		return true
-	}
-
-	err := p.Client.ListHostedZonesPages(&route53.ListHostedZonesInput{}, f)
-	if err != nil {
-		return zones, err
-	}
-
-	return zones, nil
-}
-
-// Zone returns a single zone given a DNS name.
-func (p *AWSProvider) Zone(dnsName string) (*route53.HostedZone, error) {
-	params := &route53.ListHostedZonesByNameInput{
-		DNSName: aws.String(dnsName),
-	}
-
-	resp, err := p.Client.ListHostedZonesByName(params)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(resp.HostedZones) != 1 {
-		return nil, fmt.Errorf("not exactly one hosted zone found by name, got %d", len(resp.HostedZones))
-	}
-
-	return resp.HostedZones[0], nil
-}
-
-// CreateZone creates a hosted zone given a name.
-func (p *AWSProvider) CreateZone(name string) (*route53.HostedZone, error) {
-	params := &route53.CreateHostedZoneInput{
-		CallerReference: aws.String(uuid.New().String()),
-		Name:            aws.String(name),
-	}
-
-	resp, err := p.Client.CreateHostedZone(params)
-	if err != nil {
-		return nil, err
-	}
-
-	return resp.HostedZone, nil
-}
-
-// DeleteZone deletes a hosted zone given a name.
-func (p *AWSProvider) DeleteZone(name string) error {
-	params := &route53.DeleteHostedZoneInput{
-		Id: aws.String(name),
-	}
-
-	_, err := p.Client.DeleteHostedZone(params)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
 // Records returns the list of records in a given hosted zone.
 func (p *AWSProvider) Records(zone string) ([]*endpoint.Endpoint, error) {
-	hostedZone, err := p.Zone(zone)
-	if err != nil {
-		return nil, err
-	}
-
-	params := &route53.ListResourceRecordSetsInput{
-		HostedZoneId: hostedZone.Id,
-	}
-
 	endpoints := []*endpoint.Endpoint{}
 
 	f := func(resp *route53.ListResourceRecordSetsOutput, lastPage bool) (shouldContinue bool) {
@@ -166,8 +84,11 @@ func (p *AWSProvider) Records(zone string) ([]*endpoint.Endpoint, error) {
 		return true
 	}
 
-	err = p.Client.ListResourceRecordSetsPages(params, f)
-	if err != nil {
+	params := &route53.ListResourceRecordSetsInput{
+		HostedZoneId: aws.String(zone),
+	}
+
+	if err := p.Client.ListResourceRecordSetsPages(params, f); err != nil {
 		return nil, err
 	}
 
@@ -207,11 +128,6 @@ func (p *AWSProvider) submitChanges(zone string, changes []*route53.Change) erro
 		return nil
 	}
 
-	hostedZone, err := p.Zone(zone)
-	if err != nil {
-		return err
-	}
-
 	if p.DryRun {
 		for _, change := range changes {
 			log.Infof("Changing records: %s %s", aws.StringValue(change.Action), change.String())
@@ -221,14 +137,13 @@ func (p *AWSProvider) submitChanges(zone string, changes []*route53.Change) erro
 	}
 
 	params := &route53.ChangeResourceRecordSetsInput{
-		HostedZoneId: hostedZone.Id,
+		HostedZoneId: aws.String(zone),
 		ChangeBatch: &route53.ChangeBatch{
 			Changes: changes,
 		},
 	}
 
-	_, err = p.Client.ChangeResourceRecordSets(params)
-	if err != nil {
+	if _, err := p.Client.ChangeResourceRecordSets(params); err != nil {
 		return err
 	}
 
