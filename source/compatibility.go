e71cb2a492e9cb4469862b2a2b5205bd7b566749
Martin Linkhorst <martin.linkhorst+github.com@posteo.de>
2017-04-10 14:05:29 +0200

    feat(source): support services annotated with mate's annotations

---
+++ source/compatibility.go
@@ -0,0 +1,57 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package source
+
+import (
+	"k8s.io/client-go/pkg/api/v1"
+
+	"github.com/kubernetes-incubator/external-dns/endpoint"
+)
+
+const (
+	mateAnnotationKey = "zalando.org/dnsname"
+)
+
+// legacyEndpointsFromService tries to retrieve Endpoints from Services
+// annotated with legacy annotations.
+func legacyEndpointsFromService(svc *v1.Service) []*endpoint.Endpoint {
+	return legacyEndpointsFromMateService(svc)
+}
+
+// legacyEndpointsFromMateService tries to retrieve Endpoints from Services
+// annotated with Mate's annotation semantics.
+func legacyEndpointsFromMateService(svc *v1.Service) []*endpoint.Endpoint {
+	var endpoints []*endpoint.Endpoint
+
+	// Get the desired hostname of the service from the annotation.
+	hostname, exists := svc.Annotations[mateAnnotationKey]
+	if !exists {
+		return nil
+	}
+
+	// Create a corresponding endpoint for each configured external entrypoint.
+	for _, lb := range svc.Status.LoadBalancer.Ingress {
+		if lb.IP != "" {
+			endpoints = append(endpoints, endpoint.NewEndpoint(hostname, lb.IP))
+		}
+		if lb.Hostname != "" {
+			endpoints = append(endpoints, endpoint.NewEndpoint(hostname, lb.Hostname))
+		}
+	}
+
+	return endpoints
+}
