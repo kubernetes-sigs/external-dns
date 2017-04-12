644206c9525574c5a8418dbf6c4643608acd1bec
Martin Linkhorst <linki@users.noreply.github.com>
2017-04-12 12:49:11 +0200

    allow plans to be parameterized via policies (#127)
    * feat(plan): allow plans to be parameterized via policies
    
    * fix(controller): set a policy in the controller tests
    
    * chore: allow go tip to fail
    
    * chore: update changelog to include policies
    
    * fix(plan): store a pointer to changes
    
    * fix(controller): don't get address of a pointer

--- plan/plan.go
+++ plan/plan.go
@@ -27,9 +27,11 @@ type Plan struct {
 	Current []*endpoint.Endpoint
 	// List of desired records
 	Desired []*endpoint.Endpoint
+	// Policy under which the desired changes are calculated
+	Policy Policy
 	// List of changes necessary to move towards desired state
 	// Populated after calling Calculate()
-	Changes Changes
+	Changes *Changes
 }
 
 // Changes holds lists of actions to be executed by dns providers
@@ -45,9 +47,10 @@ type Changes struct {
 }
 
 // Calculate computes the actions needed to move current state towards desired
-// state. It returns a copy of Plan with the changes populated.
+// state. It then passes those changes to the current policy for further
+// processing. It returns a copy of Plan with the changes populated.
 func (p *Plan) Calculate() *Plan {
-	changes := Changes{}
+	changes := &Changes{}
 
 	// Ensure all desired records exist. For each desired record make sure it's
 	// either created or updated.
@@ -76,6 +79,9 @@ func (p *Plan) Calculate() *Plan {
 			changes.Delete = append(changes.Delete, current)
 		}
 	}
+
+	// Apply policy to list of changes.
+	changes = p.Policy.Apply(changes)
 
 	plan := &Plan{
 		Current: p.Current,
