316f697a5a6964b8565584ebd05e7354c68af4ed
Martin Linkhorst <linki@users.noreply.github.com>
2017-06-07 13:33:02 +0200

    chore: update all vendored dependencies (#220)

--- vendor/golang.org/x/sys/windows/zsyscall_windows.go
+++ vendor/golang.org/x/sys/windows/zsyscall_windows.go
@@ -78,7 +78,6 @@ var (
 	procSetFilePointer                     = modkernel32.NewProc("SetFilePointer")
 	procCloseHandle                        = modkernel32.NewProc("CloseHandle")
 	procGetStdHandle                       = modkernel32.NewProc("GetStdHandle")
-	procSetStdHandle                       = modkernel32.NewProc("SetStdHandle")
 	procFindFirstFileW                     = modkernel32.NewProc("FindFirstFileW")
 	procFindNextFileW                      = modkernel32.NewProc("FindNextFileW")
 	procFindClose                          = modkernel32.NewProc("FindClose")
@@ -621,22 +620,10 @@ func CloseHandle(handle Handle) (err error) {
 	return
 }
 
-func GetStdHandle(stdhandle uint32) (handle Handle, err error) {
+func GetStdHandle(stdhandle int) (handle Handle, err error) {
 	r0, _, e1 := syscall.Syscall(procGetStdHandle.Addr(), 1, uintptr(stdhandle), 0, 0)
 	handle = Handle(r0)
 	if handle == InvalidHandle {
-		if e1 != 0 {
-			err = errnoErr(e1)
-		} else {
-			err = syscall.EINVAL
-		}
-	}
-	return
-}
-
-func SetStdHandle(stdhandle uint32, handle Handle) (err error) {
-	r1, _, e1 := syscall.Syscall(procSetStdHandle.Addr(), 2, uintptr(stdhandle), uintptr(handle), 0)
-	if r1 == 0 {
 		if e1 != 0 {
 			err = errnoErr(e1)
 		} else {
