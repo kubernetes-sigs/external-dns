387e3bdeb527a78dabcbda2f88095408c995b6f3
Till Klocke <till.klocke@gmail.com>
2017-12-05 14:39:19 +0100

    Endpoint.Target is now Endpoint.Targets. This is its own type representing mutliple targets for a single DNS name while adding some convenience for sorting and comparing

--- endpoint/endpoint.go
+++ endpoint/endpoint.go
@@ -18,6 +18,7 @@ package endpoint
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 )
 
@@ -38,12 +39,79 @@ func (ttl TTL) IsConfigured() bool {
 	return ttl > 0
 }
 
+type Targets []string
+
+func NewTargets(target ...string) Targets {
+	t := make(Targets, 0, len(target))
+	t = append(t, target...)
+	return t
+}
+
+func (t Targets) Add(target string) {
+	if t == nil {
+		t = make(Targets, 0, 10)
+	}
+	t = append(t, strings.TrimSuffix(target, "."))
+}
+
+func (t Targets) String() string {
+	return strings.Join(t, ";")
+}
+
+func (t Targets) Len() int {
+	return len(t)
+}
+
+func (t Targets) Less(i, j int) bool {
+	return t[i] < t[j]
+}
+
+func (t Targets) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
+
+func (t Targets) Same(o Targets) bool {
+	if len(t) != len(o) {
+		return false
+	}
+	sort.Stable(t)
+	sort.Stable(o)
+
+	for i, e := range t {
+		if e != o[i] {
+			return false
+		}
+	}
+	return true
+}
+
+// FIXME We really need to define under which circumstances a list Targets is considered 'less'
+// than another.
+func (t Targets) IsLess(o Targets) bool {
+	if len(t) < len(o) {
+		return true
+	}
+	if len(t) > len(o) {
+		return false
+	}
+
+	sort.Sort(t)
+	sort.Sort(o)
+
+	for i, e := range t {
+		if e != o[i] {
+			return e < o[i]
+		}
+	}
+	return false
+}
+
 // Endpoint is a high-level way of a connection between a service and an IP
 type Endpoint struct {
 	// The hostname of the DNS record
 	DNSName string
-	// The target the DNS record points to
-	Target string
+	// The targets the DNS record points to
+	Targets Targets
 	// RecordType type of record, e.g. CNAME, A, TXT etc
 	RecordType string
 	// TTL for the record
@@ -61,7 +129,7 @@ func NewEndpoint(dnsName, target, recordType string) *Endpoint {
 func NewEndpointWithTTL(dnsName, target, recordType string, ttl TTL) *Endpoint {
 	return &Endpoint{
 		DNSName:    strings.TrimSuffix(dnsName, "."),
-		Target:     strings.TrimSuffix(target, "."),
+		Targets:    Targets{strings.TrimSuffix(target, ".")},
 		RecordType: recordType,
 		Labels:     NewLabels(),
 		RecordTTL:  ttl,
@@ -69,5 +137,5 @@ func NewEndpointWithTTL(dnsName, target, recordType string, ttl TTL) *Endpoint {
 }
 
 func (e *Endpoint) String() string {
-	return fmt.Sprintf("%s %d IN %s %s", e.DNSName, e.RecordTTL, e.RecordType, e.Target)
+	return fmt.Sprintf("%s %d IN %s %s", e.DNSName, e.RecordTTL, e.RecordType, e.Targets)
 }
