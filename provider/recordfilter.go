51ce44512281a5a31b909001b47957299db1b8d0
Gary Kramlich <grim@reaperworld.com>
2018-06-08 13:40:55 -0500

    Add SRV as a supported record type

--- provider/recordfilter.go
+++ provider/recordfilter.go
@@ -17,10 +17,10 @@ limitations under the License.
 package provider
 
 // supportedRecordType returns true only for supported record types.
-// Currently only A, CNAME and TXT record types are supported.
+// Currently A, CNAME, SRV, and TXT record types are supported.
 func supportedRecordType(recordType string) bool {
 	switch recordType {
-	case "A", "CNAME", "TXT":
+	case "A", "CNAME", "SRV", "TXT":
 		return true
 	default:
 		return false
