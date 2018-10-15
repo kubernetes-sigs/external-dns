1b42c0ae0cdbe87fc1bd082bdda3cca729897272
shashidharatd <shashidhara.td@huawei.com>
2018-10-15 11:43:19 +0530

    Fix nil map access of endpoint labels

--- registry/txt.go
+++ registry/txt.go
@@ -98,13 +98,11 @@ func (im *TXTRegistry) Records() ([]*endpoint.Endpoint, error) {
 	}
 
 	for _, ep := range endpoints {
+		ep.Labels = endpoint.NewLabels()
 		if labels, ok := labelMap[ep.DNSName]; ok {
 			for k, v := range labels {
 				ep.Labels[k] = v
 			}
-		} else {
-			//this indicates that owner could not be identified, as there is no corresponding TXT record
-			ep.Labels = endpoint.NewLabels()
 		}
 	}
 
