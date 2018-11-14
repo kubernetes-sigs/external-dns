41624a84a3069ac63a18ea987e38d63728ab150b
Nick Jüttner <nick@zalando.de>
2018-11-14 14:53:12 +0100

    adding kubernetes adder

--- scripts/update_route53_k8s_txt_owner.py
+++ scripts/update_route53_k8s_txt_owner.py
@@ -1,12 +1,17 @@
-# Copyright 2017 The Kubernetes Authors.
+#!/usr/bin/env python
+
+# Copyright 2018 The Kubernetes Authors.
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
+# See the License for the specific language governing permissions and
 # limitations under the License.
 
 # This is a script that we wrote to try to help the migration over to using external-dns.  
@@ -17,8 +22,9 @@
 # to only look at one or the other if needed.
 #
 # pip install kubernetes boto3
-from kubernetes import client, config
+
 import boto3
+from kubernetes import client, config
 
 # replace with your hosted zone id
 hosted_zone_id = ''
