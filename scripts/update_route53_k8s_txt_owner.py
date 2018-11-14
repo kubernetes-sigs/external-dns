c6ac74a59a82941ccc99e8301f3507a7e0e66425
Nick Jüttner <nick@zalando.de>
2018-11-14 14:32:51 +0100

    adding kubernetes adder

--- scripts/update_route53_k8s_txt_owner.py
+++ scripts/update_route53_k8s_txt_owner.py
@@ -1,3 +1,14 @@
+# Copyright 2017 The Kubernetes Authors.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+# limitations under the License.
+
 # This is a script that we wrote to try to help the migration over to using external-dns.  
 # This script looks at kubernetes ingresses and services (which are the two things we have 
 # external-dns looking at) and compares them to existing TXT and A records in route53 to 
