1ed5fa6952430456bcf0efb6ce3724bfd6028f8a
Nick Jüttner <nick@zalando.de>
2018-11-20 16:30:25 +0100

    panic: assignment to entry in nil map

--- provider/google.go
+++ provider/google.go
@@ -204,12 +204,9 @@ func (p *GoogleProvider) Records() (endpoints []*endpoint.Endpoint, _ error) {
 			if !supportedRecordType(r.Type) {
 				continue
 			}
-			ep := &endpoint.Endpoint{
-				DNSName:    strings.TrimSuffix(r.Name, "."),
-				RecordType: r.Type,
-				Targets:    make(endpoint.Targets, 0, len(r.Rrdatas)),
-				RecordTTL:  endpoint.TTL(r.Ttl),
-			}
+			targets := make(endpoint.Targets, 0, len(r.Rrdatas))
+			ep := endpoint.NewEndpointWithTTL(strings.TrimSuffix(r.Name, "."), r.Type, endpoint.TTL(r.Ttl), targets...)
+
 			for _, rr := range r.Rrdatas {
 				// each page is processed sequentially, no need for a mutex here.
 				ep.Targets = append(ep.Targets, strings.TrimSuffix(rr, "."))
