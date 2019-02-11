909e376387991f3852684ec708863812bbb16680
David Schneider <dsbrng25b@gmail.com>
2019-02-11 16:46:36 +0100

    Make view configurable for infoblox provider

--- main.go
+++ main.go
@@ -151,6 +151,7 @@ func main() {
 				Password:     cfg.InfobloxWapiPassword,
 				Version:      cfg.InfobloxWapiVersion,
 				SSLVerify:    cfg.InfobloxSSLVerify,
+				View:         cfg.InfobloxView,
 				DryRun:       cfg.DryRun,
 			},
 		)
