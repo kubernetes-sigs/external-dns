ce9425e761a0ade8e99fc5c5ae8e2e40ffdf6678
twilfong <tim@wilfong.me>
2019-06-28 16:53:05 -0700

    remove comment to make travis linter happy

--- provider/aws_test.go
+++ provider/aws_test.go
@@ -1243,6 +1243,5 @@ func addZoneTags(tagMap map[string][]*route53.Tag, zoneID string, tags map[strin
 }
 
 func validateRecords(t *testing.T, records []*route53.ResourceRecordSet, expected []*route53.ResourceRecordSet) {
-  // If the two record sets have save elements, but different ordering, the test should still pass.
 	assert.ElementsMatch(t, expected, records)
 }
