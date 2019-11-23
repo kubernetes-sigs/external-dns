f576d5a61320d55e6395ef936f539ff91bcd7497
Robert Liotta <44572134+rsliotta@users.noreply.github.com>
2019-11-23 11:46:06 -0500

    Added secrets

--- init.sh
+++ init.sh
@@ -2,17 +2,17 @@
 
 export VERSION=0.91
 
-export  http_proxy="http://LAB-SVC-Inception:Welcome1@nadevproxy.logistics.corp:3128"
-export https_proxy="http://LAB-SVC-Inception:Welcome1@nadevproxy.logistics.corp:3128"
+#export  http_proxy="http://LAB-SVC-Inception:Welcome1@nadevproxy.logistics.corp:3128"
+#export https_proxy="http://LAB-SVC-Inception:Welcome1@nadevproxy.logistics.corp:3128"
 
-if [ ! -x /usr/local/bin/bzr ]; then 
-  brew install bzr
-fi
+#if [ ! -x /usr/local/bin/bzr ]; then 
+#  brew install bzr
+#fi
 
 docker rmi -f external-dns:${VERSION} || echo "No Image"
 docker rmi -f cevalogistics/external-dns:${VERSION} || echo "No Image"
 
-echo  dbc55c0b-6992-4503-a02a-b934247f6e42 | docker --debug login --password-stdin --username rsliotta 
+echo  ${docker_token} | docker --debug login --password-stdin --username ${docker_login} 
 docker build . -t external-dns:${VERSION} --no-cache
 docker tag  external-dns:${VERSION} cevalogistics/external-dns:${VERSION}
 docker push cevalogistics/external-dns:${VERSION} 
