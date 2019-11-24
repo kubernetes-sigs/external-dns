cfd0e53c3b491a812ab39d6a7bb3ed36c54ede05
liottar <robert.liotta@cevalogistics.com>
2019-11-24 16:00:58 -0500

    Fixing workflow

--- init.sh
+++ init.sh
@@ -2,18 +2,12 @@
 
 export VERSION=0.5.7.${BUILD_NUMBER}
 
-#export  http_proxy="http://LAB-SVC-Inception:Welcome1@nadevproxy.logistics.corp:3128"
-#export https_proxy="http://LAB-SVC-Inception:Welcome1@nadevproxy.logistics.corp:3128"
-
-#if [ ! -x /usr/local/bin/bzr ]; then 
-#  brew install bzr
-#fi
-
 docker rmi -f external-dns:${VERSION} || echo "No Image"
 docker rmi -f cevalogistics/external-dns:${VERSION} || echo "No Image"
 
 echo  ${docker_token} | docker --debug login --password-stdin --username ${docker_login} 
 docker build . -t external-dns:${VERSION} --no-cache
 docker tag  external-dns:${VERSION} cevalogistics/external-dns:${VERSION}
+docker tag  external-dns:${VERSION} cevalogistics/external-dns:experimental
 docker push cevalogistics/external-dns:${VERSION} 
-docker push cevalogistics/external-dns:latest
+docker push cevalogistics/external-dns:experimental
