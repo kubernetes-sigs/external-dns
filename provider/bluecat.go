839ed545b88437793a6721535b1ffd016ac84950
timcurless <tim.curless@thinkahead.com>
2020-01-09 15:50:31 -0600

    XPAAS-7664: Still a WIP. Adding zones logic, Records function, and some test (mocks) content.

--- provider/bluecat.go
+++ provider/bluecat.go
@@ -7,6 +7,7 @@ import (
   "fmt"
   "io/ioutil"
   "net/http"
+  "strconv"
   "strings"
 
   log "github.com/sirupsen/logrus"
@@ -18,9 +19,11 @@ import (
 )
 
 type bluecatConfig struct {
-  GatewayHost     string
-  GatewayUsername string
-  GatewayPassword string
+  GatewayHost          string `json:"gatewayHost" yaml:"gatewayHost"`
+  GatewayUsername      string `json:"gatewayUsername" yaml:"gatewayUsername"`
+  GatewayPassword      string `json:"gatewayPassword" yaml:"gatewayPassword"`
+  BluecatConfiguration string `json:"bluecatConfiguration" yaml:"bluecatConfiguration"`
+  View                 string `json:"dnsView" yaml:"dnsView"`
 }
 
 type BluecatProvider struct {
@@ -35,7 +38,33 @@ type IGatewayClient interface {
 }
 
 type GatewayClient struct {
-  Token string
+  Token                string
+  BluecatConfiguration string
+  View                 string
+}
+
+type BluecatZone struct {
+  Id           int
+  Name         string
+  AbsoluteName string
+  Deployable   bool
+}
+
+type BluecatHostRecord struct {
+  Id            int
+  Name          string
+  AbsoluteName  string
+  Addresses     []string
+  ReverseRecord bool
+  Type          string
+}
+
+type BluecatCNAMERecord struct {
+  Id               int
+  Name             string
+  AbsoluteName     string
+  LinkedRecordName string
+  Type             string
 }
 
 func NewBluecatProvider(configFile string, domainFilter DomainFilter, zoneIDFilter ZoneIDFilter, dryRun bool) (*BluecatProvider, error) {
@@ -56,7 +85,7 @@ func NewBluecatProvider(configFile string, domainFilter DomainFilter, zoneIDFilt
   }
   log.Printf("Gateway API token is: %s", token)
 
-  gatewayClient := NewGatewayClient(token)
+  gatewayClient := NewGatewayClient(token, cfg.BluecatConfiguration, cfg.View)
 
   provider := &BluecatProvider{
     domainFilter:  domainFilter,
@@ -67,16 +96,72 @@ func NewBluecatProvider(configFile string, domainFilter DomainFilter, zoneIDFilt
   return provider, nil
 }
 
-func NewGatewayClient(token string) GatewayClient {
-  return GatewayClient{Token: token}
+func NewGatewayClient(token, bluecatConfiguration, view string) GatewayClient {
+  return GatewayClient{
+    Token:                token,
+    BluecatConfiguration: bluecatConfiguration,
+    View:                 view,
+  }
 }
 
-func (p *BluecatProvider) Records() ([]*endpoint.Endpoint, error) {
-  return []*endpoint.Endpoint{}, nil
+func (p *BluecatProvider) Records() (endpoints []*endpoint.Endpoint, err error) {
+  zones, err := p.zones()
+  if err != nil {
+    return nil, fmt.Errorf("could not fetch zones: %s", err)
+  }
+
+  for _, zone := range zones {
+    log.Debugf("fetching records from zone '%s'", zone)
+    var resH []BluecatHostRecord
+    err = p.gatewayClient.getHostRecords(zone, &resH)
+    if err != nil {
+      return nil, fmt.Errorf("could not fetch host records for zone: '%s': %s", zone, err)
+    }
+    for _, rec := range resH {
+      for _, ip := range rec.Addresses {
+        endpoints = append(endpoints, endpoint.NewEndpoint(rec.AbsoluteName, endpoint.RecordTypeA, ip))
+      }
+    }
+
+    var resC []BluecatCNAMERecord
+    err = p.gatewayClient.getCNAMERecords(zone, &resC)
+    if err != nil {
+      return nil, fmt.Errorf("could not fetch CNAME records for zone: '%s': %s", zone, err)
+    }
+    for _, rec := range resC {
+      endpoints = append(endpoints, endpoint.NewEndpoint(rec.AbsoluteName, endpoint.RecordTypeCNAME, rec.LinkedRecordName))
+    }
+  }
+
+  log.Debugf("fetched %d records from Bluecat", len(endpoints))
+  return endpoints, nil
 }
 
 func (p *BluecatProvider) ApplyChanges(ctx context.Context, changes *plan.Changes) error {
   return nil
+}
+
+func (p *BluecatProvider) zones() ([]string, error) {
+  log.Debugf("retrieving Bluecat zones for configuration: %s, view: %s", p.gatewayClient.BluecatConfiguration, p.gatewayClient.View)
+  var zones []string
+
+  zonelist, err := p.gatewayClient.getBluecatZones()
+  if err != nil {
+    return nil, err
+  }
+
+  for  _, zone := range zonelist {
+    if !p.domainFilter.Match(zone.Name) {
+      continue
+    }
+
+    if !p.zoneIdFilter.Match(strconv.Itoa(zone.Id)) {
+      continue
+    }
+
+    zones = append(zones, zone.Name)
+  }
+  return zones, nil
 }
 
 // getBluecatGatewayToken retrieves a Bluecat Gateway API token.
@@ -110,4 +195,19 @@ func getBluecatGatewayToken(cfg bluecatConfig) (string, error) {
   // Example response: {"access_token": "BAMAuthToken: abc123"}
   // We only care about the actual token string - i.e. abc123
   return strings.Split(resJSON["access_token"], " ")[1], nil
+}
+
+// TODO
+func (c *GatewayClient) getBluecatZones() ([]BluecatZone, error) {
+  return []BluecatZone{}, nil
+}
+
+// TODO
+func (c *GatewayClient) getHostRecords(zone string, records *[]BluecatHostRecord) error {
+  return nil
+}
+
+// TODO
+func (c *GatewayClient) getCNAMERecords(zone string, records *[]BluecatCNAMERecord) error {
+  return nil
 }
--- provider/bluecat_test.go
+++ provider/bluecat_test.go
@@ -1,1 +1,48 @@
 package provider
+
+import (
+	"strings"
+	"testing"
+)
+
+type mockGatewayClient struct {
+	mockBluecatZones *[]BluecatZone
+	mockBluecatHosts *[]BluecatHostRecord
+	mockBluecatCNAMEs *[]BluecatCNAMERecord
+}
+
+func (c *mockGatewayClient)
+
+func createMockBluecatZone(fqdn string) BluecatZone {
+	return BluecatZone{
+		AbsoluteName: fqdn,
+		Name:         strings.Split(fqdn, ".")[0],
+		Deployable:   true,
+	}
+}
+
+func newBluecatProvider(domainFilter DomainFilter, zoneIDFilter ZoneIDFilter, dryRun bool, client GatewayClient) *BluecatProvider {
+	return &BluecatProvider{
+		domainFilter: domainFilter,
+		zoneIdFilter: zoneIDFilter,
+		dryRun: dryRun,
+		gatewayClient: client,
+	}
+}
+
+func TestBluecatRecords(t *testing.T) {
+	client := mockGatewayClient{
+		mockBluecatZones:  &[]BluecatZone{
+			createMockBluecatZone("example.com"),
+		},
+		mockBluecatHosts:  &[]BluecatHostRecord{},
+		mockBluecatCNAMEs: &[]BluecatCNAMERecord{},
+	}
+
+	provider := newBluecatProvider(NewDomainFilter([]string{"example.com"}), NewZoneIDFilter([]string("")), true, client)
+
+	actual, err := provider.Records()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
