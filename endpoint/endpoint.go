0127df6a3b2e8eb5fa4b461e2111f9641a6dec8d
Amit Ben Ami <benami.amit1@gmail.com>
2020-12-10 17:37:37 +0200

    Adding a function to check if a record type is supported

--- endpoint/endpoint.go
+++ endpoint/endpoint.go
@@ -37,6 +37,15 @@ const (
 	RecordTypeNS = "NS"
 )
 
+func IsRecordTypeSupported(t string) bool {
+	switch t {
+	case RecordTypeA, RecordTypeCNAME, RecordTypeTXT, RecordTypeSRV, RecordTypeNS:
+		return true
+	default:
+		return true
+	}
+}
+
 // TTL is a structure defining the TTL of a DNS record
 type TTL int64
 
