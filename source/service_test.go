98284a94a5ed3eecdf8638c44e4ada9d8248e14d
Allen Porter <allen@thebends.org>
2021-03-31 23:47:21 -0700

    Add unit test that exercises behavior of headless endpoint with no TargetRef.

--- source/service_test.go
+++ source/service_test.go
@@ -2423,6 +2423,7 @@ func TestHeadlessServicesHostIP(t *testing.T) {
 		podnames                 []string
 		hostnames                []string
 		podsReady                []bool
+		targetRefs               []*v1.ObjectReference
 		publishNotReadyAddresses bool
 		expected                 []*endpoint.Endpoint
 		expectError              bool
@@ -2449,6 +2450,10 @@ func TestHeadlessServicesHostIP(t *testing.T) {
 			[]string{"foo-0", "foo-1"},
 			[]string{"foo-0", "foo-1"},
 			[]bool{true, true},
+			[]*v1.ObjectReference{
+				{APIVersion: "", Kind: "Pod", Name: "foo-0"},
+				{APIVersion: "", Kind: "Pod", Name: "foo-1"},
+			},
 			false,
 			[]*endpoint.Endpoint{
 				{DNSName: "foo-0.service.example.org", Targets: endpoint.Targets{"1.1.1.1"}},
@@ -2479,6 +2484,10 @@ func TestHeadlessServicesHostIP(t *testing.T) {
 			[]string{"foo-0", "foo-1"},
 			[]string{"foo-0", "foo-1"},
 			[]bool{true, true},
+			[]*v1.ObjectReference{
+				{APIVersion: "", Kind: "Pod", Name: "foo-0"},
+				{APIVersion: "", Kind: "Pod", Name: "foo-1"},
+			},
 			false,
 			[]*endpoint.Endpoint{},
 			false,
@@ -2506,6 +2515,10 @@ func TestHeadlessServicesHostIP(t *testing.T) {
 			[]string{"foo-0", "foo-1"},
 			[]string{"foo-0", "foo-1"},
 			[]bool{true, true},
+			[]*v1.ObjectReference{
+				{APIVersion: "", Kind: "Pod", Name: "foo-0"},
+				{APIVersion: "", Kind: "Pod", Name: "foo-1"},
+			},
 			false,
 			[]*endpoint.Endpoint{
 				{DNSName: "foo-0.service.example.org", Targets: endpoint.Targets{"1.1.1.1"}, RecordTTL: endpoint.TTL(1)},
@@ -2536,6 +2549,10 @@ func TestHeadlessServicesHostIP(t *testing.T) {
 			[]string{"foo-0", "foo-1"},
 			[]string{"foo-0", "foo-1"},
 			[]bool{true, false},
+			[]*v1.ObjectReference{
+				{APIVersion: "", Kind: "Pod", Name: "foo-0"},
+				{APIVersion: "", Kind: "Pod", Name: "foo-1"},
+			},
 			false,
 			[]*endpoint.Endpoint{
 				{DNSName: "foo-0.service.example.org", Targets: endpoint.Targets{"1.1.1.1"}},
@@ -2565,6 +2582,10 @@ func TestHeadlessServicesHostIP(t *testing.T) {
 			[]string{"foo-0", "foo-1"},
 			[]string{"foo-0", "foo-1"},
 			[]bool{true, false},
+			[]*v1.ObjectReference{
+				{APIVersion: "", Kind: "Pod", Name: "foo-0"},
+				{APIVersion: "", Kind: "Pod", Name: "foo-1"},
+			},
 			true,
 			[]*endpoint.Endpoint{
 				{DNSName: "foo-0.service.example.org", Targets: endpoint.Targets{"1.1.1.1"}},
@@ -2595,10 +2616,41 @@ func TestHeadlessServicesHostIP(t *testing.T) {
 			[]string{"foo-0", "foo-1"},
 			[]string{"", ""},
 			[]bool{true, true},
+			[]*v1.ObjectReference{
+				{APIVersion: "", Kind: "Pod", Name: "foo-0"},
+				{APIVersion: "", Kind: "Pod", Name: "foo-1"},
+			},
 			false,
 			[]*endpoint.Endpoint{
 				{DNSName: "service.example.org", Targets: endpoint.Targets{"1.1.1.1", "1.1.1.2"}},
 			},
+			false,
+		},
+		{
+			"annotated Headless services without a targetRef has no endpoints",
+			"",
+			"testing",
+			"foo",
+			v1.ServiceTypeClusterIP,
+			"",
+			"",
+			false,
+			map[string]string{"component": "foo"},
+			map[string]string{
+				hostnameAnnotationKey: "service.example.org",
+			},
+			v1.ClusterIPNone,
+			[]string{"1.1.1.1"},
+			map[string]string{
+				"component": "foo",
+			},
+			[]string{},
+			[]string{"foo-0"},
+			[]string{"foo-0"},
+			[]bool{true, true},
+			[]*v1.ObjectReference{nil},
+			false,
+			[]*endpoint.Endpoint{},
 			false,
 		},
 	} {
@@ -2648,11 +2700,7 @@ func TestHeadlessServicesHostIP(t *testing.T) {
 
 				address := v1.EndpointAddress{
 					IP: "4.3.2.1",
-					TargetRef: &v1.ObjectReference{
-						APIVersion: "",
-						Kind:       "Pod",
-						Name:       podname,
-					},
+					TargetRef: tc.targetRefs[i],
 				}
 				if tc.podsReady[i] {
 					addresses = append(addresses, address)
