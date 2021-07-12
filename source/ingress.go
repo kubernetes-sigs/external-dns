65bc36c54d3b04d355b66b1edb0698815dc853f4
bechampion <garciaj.uk@gmail.com>
2021-07-12 10:50:24 +0100

    Adding trimSuffix

--- source/ingress.go
+++ source/ingress.go
@@ -73,6 +73,7 @@ func NewIngressSource(kubeClient kubernetes.Interface, namespace, annotationFilt
 	if fqdnTemplate != "" {
 		tmpl, err = template.New("endpoint").Funcs(template.FuncMap{
 			"trimPrefix": strings.TrimPrefix,
+			"trimSuffix": strings.TrimSuffix,
 		}).Parse(fqdnTemplate)
 		if err != nil {
 			return nil, err
