f651a209785191e0bbc9df8b859d8c6aed5860f4
Andy Bursavich <abursavich@gmail.com>
2021-09-07 19:00:55 -0700

    gateway-tlsroute: add source

--- source/store.go
+++ source/store.go
@@ -229,6 +229,8 @@ func BuildWithConfig(ctx context.Context, source string, p ClientGenerator, cfg 
 		return NewPodSource(ctx, client, cfg.Namespace, cfg.Compatibility)
 	case "gateway-httproute":
 		return NewGatewayHTTPRouteSource(p, cfg)
+	case "gateway-tlsroute":
+		return NewGatewayTLSRouteSource(p, cfg)
 	case "istio-gateway":
 		kubernetesClient, err := p.KubeClient()
 		if err != nil {
