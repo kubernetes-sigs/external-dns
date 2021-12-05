cef411b3f5d2049dcd31e259bb18382e5915438b
John Gardiner Myers <jgmyers@proofpoint.com>
2021-12-04 23:59:00 -0800

    Add AAAA to supported record types

--- provider/recordfilter.go
+++ provider/recordfilter.go
@@ -17,10 +17,10 @@ limitations under the License.
 package provider
 
 // SupportedRecordType returns true only for supported record types.
-// Currently A, CNAME, SRV, TXT and NS record types are supported.
+// Currently A, AAAA, CNAME, SRV, TXT and NS record types are supported.
 func SupportedRecordType(recordType string) bool {
 	switch recordType {
-	case "A", "CNAME", "SRV", "TXT", "NS":
+	case "A", "AAAA", "CNAME", "SRV", "TXT", "NS":
 		return true
 	default:
 		return false
