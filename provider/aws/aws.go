4f9811b00da64866ce1088bb196e06330d9e7f50
Jan Sandbrink <jan.sandbrink@aroundhome.de>
2022-02-09 09:21:41 +0100

    Remove an obsolete TODO comment
    This comment belonged to a switch-statement that
    has since been removed, but it was forgotten to
    remove the corresponding comment.

--- provider/aws/aws.go
+++ provider/aws/aws.go
@@ -314,9 +314,6 @@ func (p *AWSProvider) records(ctx context.Context, zones map[string]*route53.Hos
 		for _, r := range resp.ResourceRecordSets {
 			newEndpoints := make([]*endpoint.Endpoint, 0)
 
-			// TODO(linki, ownership): Remove once ownership system is in place.
-			// See: https://github.com/kubernetes-sigs/external-dns/pull/122/files/74e2c3d3e237411e619aefc5aab694742001cdec#r109863370
-
 			if !provider.SupportedRecordType(aws.StringValue(r.Type)) {
 				continue
 			}
