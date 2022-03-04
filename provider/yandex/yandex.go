788e3e73f857e963d99d00d77086ab0a1e58922f
ArtemTrofimushkin <artemtrofimushkin@gmail.com>
2022-03-04 21:06:56 +0300

    feat(yandex): use structured logging for messages
    Signed-off-by: ArtemTrofimushkin <artemtrofimushkin@gmail.com>

--- provider/yandex/yandex.go
+++ provider/yandex/yandex.go
@@ -19,6 +19,7 @@ package yandex
 import (
 	"context"
 	"errors"
+	"fmt"
 	"strings"
 
 	log "github.com/sirupsen/logrus"
@@ -160,22 +161,24 @@ func (p *YandexProvider) ApplyChanges(ctx context.Context, changes *plan.Changes
 	})
 
 	for _, batch := range batchMap {
-		log.Infof("Would perform be batch update for zone: %s\n"+
-			"Records to delete: %s\n"+
-			"Records to create: %s\n"+
-			"Records to update: %s\n",
-			batch.ZoneName,
-			toString(batch.Deletes),
-			toString(batch.Creates),
-			toString(batch.Updates),
-		)
+		log.WithFields(log.Fields{
+			"deletes": toString(batch.Deletes),
+			"creates": toString(batch.Creates),
+			"updates": toString(batch.Updates),
+			"zone":    batch.ZoneName,
+			"zoneID":  batch.ZoneID,
+		}).Info("Would execute upsert operation")
 
 		if p.DryRun {
 			continue
 		}
 
 		if err := p.upsertRecords(ctx, batch); err != nil {
-			log.Errorf("Failed to execute upsert operation: %v", err)
+			log.WithFields(log.Fields{
+				"error":  err,
+				"zone":   batch.ZoneName,
+				"zoneID": batch.ZoneID,
+			}).Error("Failed to execute upsert operation")
 		}
 	}
 
@@ -183,7 +186,7 @@ func (p *YandexProvider) ApplyChanges(ctx context.Context, changes *plan.Changes
 }
 
 func (p *YandexProvider) zones(ctx context.Context) ([]*dnsInt.DnsZone, error) {
-	log.Debugf("Retrieving Yandex DNS zones for folder: %s.", p.FolderID)
+	log.WithFields(log.Fields{"folder": p.FolderID}).Debug("Retrieving Yandex DNS zones for folder")
 
 	iterator := p.client.ZoneIterator(ctx, &dnsInt.ListDnsZonesRequest{
 		FolderId: p.FolderID,
@@ -195,12 +198,18 @@ func (p *YandexProvider) zones(ctx context.Context) ([]*dnsInt.DnsZone, error) {
 		zone := iterator.Value()
 
 		if !p.DomainFilter.Match(zone.Zone) || !p.ZoneIDFilter.Match(zone.Id) {
-			log.Debugf("Skipping zone '%s' because of Domain And ZoneId filters", zone.Zone)
+			log.WithFields(log.Fields{
+				"zone":   zone.Zone,
+				"zoneID": zone.Id,
+			}).Debug("Skipping zone because of Domain and ZoneId filters")
 			continue
 		}
 
 		if !p.ZoneNameFilter.Match(zone.Zone) {
-			log.Debugf("Skipping zone '%s' because of ZoneName filter", zone.Zone)
+			log.WithFields(log.Fields{
+				"zone":   zone.Zone,
+				"zoneID": zone.Id,
+			}).Debug("Skipping zone because of ZoneName filter")
 			continue
 		}
 
@@ -211,12 +220,18 @@ func (p *YandexProvider) zones(ctx context.Context) ([]*dnsInt.DnsZone, error) {
 		return nil, err
 	}
 
-	log.Debugf("Found %d Yandex DNS zone(s).", len(zones))
+	log.WithFields(log.Fields{
+		"folder": p.FolderID,
+		"zones":  len(zones),
+	}).Debug("Found Yandex DNS zone(s) for folder")
 	return zones, nil
 }
 
 func (p *YandexProvider) records(ctx context.Context, zoneName, zoneID string) ([]*dnsInt.RecordSet, error) {
-	log.Debugf("Retrieving Yandex DNS records for zone '%s'.", zoneName)
+	log.WithFields(log.Fields{
+		"zone":   zoneName,
+		"zoneID": zoneID,
+	}).Debug("Retrieving Yandex DNS records for zone")
 
 	iterator := p.client.RecordSetIterator(ctx, &dnsInt.ListDnsZoneRecordSetsRequest{
 		DnsZoneId: zoneID,
@@ -228,22 +243,31 @@ func (p *YandexProvider) records(ctx context.Context, zoneName, zoneID string) (
 		record := iterator.Value()
 
 		if record == nil {
-			log.Debugf("Skipping invalid nil record")
+			log.Debug("Skipping invalid nil record")
 			continue
 		}
 
 		if !provider.SupportedRecordType(record.Type) {
-			log.Debugf("Skipping record because of not supported type")
+			log.WithFields(log.Fields{
+				"record":     record.Name,
+				"recordType": record.Type,
+			}).Debug("Skipping record because of not supported type")
 			continue
 		}
 
 		if len(p.ZoneNameFilter.Filters) > 0 && !p.DomainFilter.Match(record.Name) {
-			log.Debugf("Skipping return of record %s because it was filtered out by the specified --domain-filter", record.Name)
+			log.WithFields(log.Fields{
+				"record":     record.Name,
+				"recordType": record.Type,
+			}).Debug("Skipping return of record because it was filtered out by the specified --domain-filter")
 			continue
 		}
 
 		if record.Data == nil || len(record.Data) == 0 {
-			log.Debugf("Skipping return of record %s (%s) because it with empty targets", record.Name, record.Type)
+			log.WithFields(log.Fields{
+				"record":     record.Name,
+				"recordType": record.Type,
+			}).Debug("Skipping return of record because it with empty targets")
 			continue
 		}
 
@@ -254,17 +278,22 @@ func (p *YandexProvider) records(ctx context.Context, zoneName, zoneID string) (
 		return nil, err
 	}
 
-	log.Debugf("Found %d Yandex DNS records for zone '%s'.", len(records), zoneName)
+	log.WithFields(log.Fields{
+		"records": len(records),
+		"zone":    zoneName,
+		"zoneID":  zoneID,
+	}).Debug("Found Yandex DNS record(s) for zone")
 	return records, nil
 }
 
 func (p *YandexProvider) upsertRecords(ctx context.Context, batch *upsertBatch) error {
-	log.Infof("Perform upsert operation for zone '%s'. Deletes: %d, Updates: %d, Creates: %d",
-		batch.ZoneName,
-		len(batch.Deletes),
-		len(batch.Updates),
-		len(batch.Creates),
-	)
+	log.WithFields(log.Fields{
+		"zone":         batch.ZoneName,
+		"zoneID":       batch.ZoneID,
+		"createsCount": len(batch.Deletes),
+		"updatesCount": len(batch.Updates),
+		"deletesCount": len(batch.Creates),
+	}).Info("Perform upsert operation for zone")
 
 	_, err := p.client.UpsertRecordSets(ctx,
 		&dnsInt.UpsertRecordSetsRequest{
@@ -276,7 +305,10 @@ func (p *YandexProvider) upsertRecords(ctx context.Context, batch *upsertBatch) 
 	)
 
 	if err != nil {
-		log.Errorf("Failed to perform upsert operation for zone '%s'", batch.ZoneName)
+		log.WithFields(log.Fields{
+			"zone":   batch.ZoneName,
+			"zoneID": batch.ZoneID,
+		}).Error("Failed to perform upsert operation for zone")
 		return err
 	}
 	return nil
@@ -284,7 +316,7 @@ func (p *YandexProvider) upsertRecords(ctx context.Context, batch *upsertBatch) 
 
 func toEndpoint(record *dnsInt.RecordSet) *endpoint.Endpoint {
 	if record == nil {
-		log.Errorf("Skipping invalid record set with nil definition")
+		log.Error("Skipping invalid record set with nil definition")
 		return nil
 	}
 
@@ -298,7 +330,7 @@ func toEndpoint(record *dnsInt.RecordSet) *endpoint.Endpoint {
 
 func toRecordSet(ep *endpoint.Endpoint) *dnsInt.RecordSet {
 	if ep == nil {
-		log.Errorf("Skipping invalid endpoint with nil definition")
+		log.Error("Skipping invalid endpoint with nil definition")
 		return nil
 	}
 
@@ -316,12 +348,11 @@ func toRecordSet(ep *endpoint.Endpoint) *dnsInt.RecordSet {
 }
 
 func toString(records []*dnsInt.RecordSet) string {
-	message := ""
+	var message strings.Builder
 
 	for _, record := range records {
-		message += record.Name
-		message += ","
+		message.WriteString(fmt.Sprintf("%s (%s); ", record.Name, record.Type))
 	}
 
-	return message
+	return message.String()
 }
