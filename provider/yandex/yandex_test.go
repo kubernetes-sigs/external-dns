d8d7f6c90444d37aafa228e7a4fffa2f5fb87bf3
ArtemTrofimushkin <artemtrofimushkin@gmail.com>
2022-03-13 14:08:15 +0300

    chore(yandex): add tests for ApplyChanges
    Signed-off-by: ArtemTrofimushkin <artemtrofimushkin@gmail.com>

--- provider/yandex/yandex_test.go
+++ provider/yandex/yandex_test.go
@@ -25,7 +25,7 @@ import (
 
 	"sigs.k8s.io/external-dns/endpoint"
 	"sigs.k8s.io/external-dns/internal/testutils"
-	"sigs.k8s.io/external-dns/provider"
+	"sigs.k8s.io/external-dns/plan"
 )
 
 func assertEndpointsAreSame(t *testing.T, expected, actual []*endpoint.Endpoint) {
@@ -71,4 +71,65 @@ func TestYandexRecordWithDomainFilter(t *testing.T) {
 		{DNSName: "test.yandex.io", Targets: []string{"1.2.3.4"}, RecordType: "A", RecordTTL: 10},
 		{DNSName: "another.yandex.io", Targets: []string{"test2.yandex.io"}, RecordType: "CNAME", RecordTTL: 10},
 	}, actual)
+}
+
+func TestYandexApplyChangesWithDryRun(t *testing.T) {
+	f := newFixture().
+		WithZoneRecords("yandex.io.",
+			&dnsProto.RecordSet{Name: "internal.yandex.io.", Type: "A", Ttl: 10, Data: []string{"1.2.3.4"}},
+			&dnsProto.RecordSet{Name: "stub.yandex.io.", Type: "CNAME", Ttl: 10, Data: []string{"test2.yandex.io"}},
+		).
+		WithDryRun()
+	changes := &plan.Changes{
+		Create: []*endpoint.Endpoint{
+			endpoint.NewEndpointWithTTL("new.yandex.io", "A", 10, "1.2.3.5"),
+		},
+		Delete: []*endpoint.Endpoint{
+			endpoint.NewEndpointWithTTL("internal.yandex.io", "A", 10, "1.2.3.4"),
+		},
+		UpdateOld: []*endpoint.Endpoint{
+			endpoint.NewEndpointWithTTL("stub.yandex.io", "CNAME", 10, "test2.yandex.io"),
+		},
+		UpdateNew: []*endpoint.Endpoint{
+			endpoint.NewEndpointWithTTL("stub.yandex.io", "CNAME", 10, "google.com"),
+		},
+	}
+
+	err := f.Provider().ApplyChanges(context.Background(), changes)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.True(t, f.Client().upsertRequest == nil)
+}
+
+func TestYandexApplyChanges(t *testing.T) {
+	f := newFixture().
+		WithZoneRecords("yandex.io.",
+			&dnsProto.RecordSet{Name: "internal.yandex.io.", Type: "A", Ttl: 10, Data: []string{"1.2.3.4"}},
+			&dnsProto.RecordSet{Name: "stub.yandex.io.", Type: "CNAME", Ttl: 10, Data: []string{"test2.yandex.io"}},
+		)
+	changes := &plan.Changes{
+		Create: []*endpoint.Endpoint{
+			endpoint.NewEndpointWithTTL("new.yandex.io", "A", 10, "1.2.3.5"),
+		},
+		Delete: []*endpoint.Endpoint{
+			endpoint.NewEndpointWithTTL("internal.yandex.io", "A", 10, "1.2.3.4"),
+		},
+		UpdateOld: []*endpoint.Endpoint{
+			endpoint.NewEndpointWithTTL("stub.yandex.io", "CNAME", 10, "test2.yandex.io"),
+		},
+		UpdateNew: []*endpoint.Endpoint{
+			endpoint.NewEndpointWithTTL("stub.yandex.io", "CNAME", 10, "google.com"),
+		},
+	}
+
+	err := f.Provider().ApplyChanges(context.Background(), changes)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	assertEndpointsAreSame(t, changes.Delete, f.Client().upsertRequest.Deletions)
+	assertEndpointsAreSame(t, changes.Create, f.Client().upsertRequest.Merges)
+	assertEndpointsAreSame(t, changes.UpdateNew, f.Client().upsertRequest.Replacements)
 }
