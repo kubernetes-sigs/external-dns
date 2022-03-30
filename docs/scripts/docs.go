ee3a20b927102aa7438d7d67c11244b712a4cfe9
renehernandez <7692501+renehernandez@users.noreply.github.com>
2022-03-30 11:07:21 -0400

    Fix lint issues in scripts/docs.go

--- docs/scripts/docs.go
+++ docs/scripts/docs.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package main
 
 import (
@@ -24,7 +40,6 @@ func removeLinkPrefixInIndex() {
 	if _, err := f.WriteString(updatedContent); err != nil {
 		log.Fatalf("Failed writing links update to index.md. Original error: %s", err)
 	}
-
 }
 
 func main() {
