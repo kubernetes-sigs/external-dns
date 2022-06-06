5108dea465034cad475ce1ca62a1ceec3b823bd3
Will Marchesi <william.marchesi@aveshasystems.com>
2022-06-06 12:39:56 -0400

    NewStackPathProvider() Test

--- provider/stackpath/stackpath_test.go
+++ provider/stackpath/stackpath_test.go
@@ -15,3 +15,73 @@ limitations under the License.
 */
 
 package stackpath
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"sigs.k8s.io/external-dns/endpoint"
+	"sigs.k8s.io/external-dns/provider"
+)
+
+func TestNewStackPathProvider(t *testing.T) {
+	stackPathConfig := &StackPathConfig{
+		Context:      context.Background(),
+		DomainFilter: endpoint.NewDomainFilter(nil),
+		ZoneIDFilter: provider.NewZoneIDFilter(nil),
+		DryRun:       false,
+		Testing:      true,
+	}
+
+	_, err := NewStackPathProvider(*stackPathConfig)
+	if err == nil {
+		t.Fatalf("Expected to fail without a valid CLIENT_ID, CLIENT_SECRET, and STACK_ID")
+	}
+
+	_ = os.Setenv("STACKPATH_CLIENT_ID", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+	_ = os.Setenv("STACKPATH_CLIENT_SECRET", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+	_ = os.Setenv("STACKPATH_STACK_ID", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+
+	_, err = NewStackPathProvider(*stackPathConfig)
+	if err.Error() != "Error obtaining oauth2 token: 404 Not Found" {
+		t.Fatalf("%v", err)
+	}
+}
+
+// func TestTestTest(t *testing.T) {
+// 	provider := &StackPathProvider{
+// 		client:       &dns.APIClient{},
+// 		context:      context.Background(),
+// 		domainFilter: endpoint.DomainFilter{},
+// 		zoneIdFilter: provider.ZoneIDFilter{},
+// 		stackId:      "",
+// 		dryRun:       true,
+//      testing:      true
+// 	}
+
+// 	a, b, c := provider.getZoneRecords("test.com")
+
+// 	if a != dns.ZoneGetZoneRecordsResponse {
+// 	}
+// 	{
+// 		t.Fatalf("Expected empty response")
+// 	}
+// 	spew.Dump(b)
+// 	spew.Dump(c)
+
+// }
+
+// func TestStackPathRecords(t *testing.T) {
+// 	mocked := mockStackPathProvider{}
+
+// 	provider := &StackPathProvider{
+// 		client: mocked,
+// 	}
+
+// 	_, err := provider.zones()
+// 	if err != nil {
+// 		t.Fatalf("%v", err)
+// 	}
+
+// }
