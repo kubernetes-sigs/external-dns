157fe0962ca9971f380e6eaf2b1ae4b1019d81c8
Will Marchesi <will@marchesi.io>
2022-06-17 09:18:27 -0400

    Increase test coverage

--- provider/stackpath/stackpath_test.go
+++ provider/stackpath/stackpath_test.go
@@ -18,6 +18,7 @@ package stackpath
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"sort"
 	"testing"
@@ -49,26 +50,41 @@ func TestNewStackPathProvider(t *testing.T) {
 	}
 
 	_, err := NewStackPathProvider(*stackPathConfig)
-	if err == nil {
-		t.Fatalf("Expected to fail without a valid CLIENT_ID, CLIENT_SECRET, and STACK_ID")
-	}
-
+	assert.Equal(t, fmt.Errorf("STACKPATH_CLIENT_ID environment variable is not set"), err)
 	_ = os.Setenv("STACKPATH_CLIENT_ID", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-	_ = os.Setenv("STACKPATH_CLIENT_SECRET", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+
+	_, err = NewStackPathProvider(*stackPathConfig)
+	assert.Equal(t, fmt.Errorf("STACKPATH_CLIENT_SECRET environment variable is not set"), err)
+	_ = os.Setenv("STACKPATH_CLIENT_SECRET", "IS_SET")
+
+	_, err = NewStackPathProvider(*stackPathConfig)
+	assert.Equal(t, fmt.Errorf("STACKPATH_STACK_ID environment variable is not set"), err)
 	_ = os.Setenv("STACKPATH_STACK_ID", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 
 	_, err = NewStackPathProvider(*stackPathConfig)
-	if err.Error() != "Error obtaining oauth2 token: 404 Not Found" {
-		t.Fatalf("%v", err)
-	}
+	assert.Equal(t, fmt.Errorf("Error obtaining oauth2 token: 404 Not Found"), err)
+	_ = os.Setenv("STACKPATH_CLIENT_SECRET", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+
+	_, err = NewStackPathProvider(*stackPathConfig)
+	assert.Equal(t, nil, err)
 }
 
 func TestZones(t *testing.T) {
+	testProvider.dryRun = false
 	zones, err := testProvider.zones()
 	assert.NoError(t, err)
 	assert.Equal(t, 1, len(zones))
 	assert.Contains(t, zones[0].GetNameservers(), "ns1.example.com")
 	assert.Equal(t, false, zones[0].GetDisabled())
+
+	testProvider.dryRun = true
+	_, err = testProvider.zones()
+	assert.Equal(t, fmt.Errorf("testing"), err)
+	testProvider.dryRun = false
+
+	testProvider.zoneIDFilter = provider.NewZoneIDFilter([]string{"TEST_ZONE_ID"})
+	_, err = testProvider.zones()
+	testProvider.zoneIDFilter = provider.NewZoneIDFilter(nil)
 }
 
 func TestGetZones(t *testing.T) {
@@ -90,6 +106,11 @@ func TestGetZoneRecords(t *testing.T) {
 	assert.Equal(t, 3, len(records))
 	assert.Equal(t, "www", records[0].GetName())
 	assert.Equal(t, "testing.com", records[2].GetData())
+
+	testProvider.dryRun = true
+	_, _, err = testProvider.getZoneRecords("")
+	assert.Equal(t, fmt.Errorf("testing"), err)
+	testProvider.dryRun = false
 }
 
 func TestRecords(t *testing.T) {
@@ -98,17 +119,32 @@ func TestRecords(t *testing.T) {
 	assert.Equal(t, 2, len(endpoints))
 	assert.GreaterOrEqual(t, len(endpoints[0].Targets), 1)
 	assert.GreaterOrEqual(t, len(endpoints[1].Targets), 1)
+
+	testProvider.dryRun = true
+	_, err = testProvider.Records(context.Background())
+	assert.Equal(t, fmt.Errorf("testing"), err)
+	testProvider.dryRun = false
 }
 
 func TestStackPathStyleRecords(t *testing.T) {
 	records, err := testProvider.StackPathStyleRecords()
 	assert.NoError(t, err)
 	assert.Equal(t, 3, len(records))
+
+	testProvider.dryRun = true
+	_, err = testProvider.StackPathStyleRecords()
+	assert.Equal(t, fmt.Errorf("testing"), err)
+	testProvider.dryRun = false
 }
 
 func TestApplyChanges(t *testing.T) {
 	err := testProvider.ApplyChanges(context.Background(), &plan.Changes{})
 	assert.NoError(t, err)
+
+	testProvider.dryRun = true
+	err = testProvider.ApplyChanges(context.Background(), &plan.Changes{})
+	assert.Equal(t, fmt.Errorf("testing"), err)
+	testProvider.dryRun = false
 }
 
 func TestRecordFromTarget(t *testing.T) {
@@ -123,14 +159,16 @@ func TestRecordFromTarget(t *testing.T) {
 	}
 
 	record, err := recordFromTarget(endpoint, "1.1.1.1", &testGetZoneZoneRecords, "one.com")
-
 	assert.NoError(t, err)
 	assert.Equal(t, "TEST_ZONE_ZONE_RECORD_ID1", record)
 
 	record, err = recordFromTarget(endpoint, "2.2.2.2", &testGetZoneZoneRecords, "one.com")
-
 	assert.NoError(t, err)
 	assert.Equal(t, "TEST_ZONE_ZONE_RECORD_ID2", record)
+
+	endpoint.DNSName = ""
+	_, err = recordFromTarget(endpoint, "3.3.3.3", &testGetZoneZoneRecords, "one.com")
+	assert.Equal(t, fmt.Errorf("record not found"), err)
 }
 
 func TestMergeEndpointsByNameType(t *testing.T) {
@@ -151,4 +189,22 @@ func TestMergeEndpointsByNameType(t *testing.T) {
 	assert.Contains(t, endpoints[1].Targets, testMergedEndpoints[1].Targets[1])
 
 	assert.Equal(t, testMergedEndpoints[0], endpoints[0])
+
+	assert.Equal(t, testMergedEndpoints, mergeEndpointsByNameType(testMergedEndpoints))
+}
+
+func TestEndpointsByZoneID(t *testing.T) {
+
+	zoneIDNameMap := provider.ZoneIDName{}
+
+	zoneResponse, _, err := testProvider.getZones()
+	assert.NoError(t, err)
+	zones := append(zoneResponse.GetZones(), badZone)
+
+	for _, zone := range zones {
+		zoneIDNameMap.Add(zone.GetId(), zone.GetDomain())
+	}
+
+	endpointByZoneIDMap := endpointsByZoneID(zoneIDNameMap, testEndpoints)
+	assert.Equal(t, 1, len(endpointByZoneIDMap))
 }
