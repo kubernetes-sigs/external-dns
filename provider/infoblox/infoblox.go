18fd5b1af6a7a0dede3e15fcfe1b7f5bd1ee9b35
Greg Sidelinger <gate@ilive4code.net>
2022-09-06 13:22:29 +0000

    Log when infoblox provider deletes a record at info
    Signed-off-by: Greg Sidelinger <sidelig@nationwide.com>
    Signed-off-by: Greg Sidelinger <gate@ilive4code.net>

--- provider/infoblox/infoblox.go
+++ provider/infoblox/infoblox.go
@@ -679,7 +679,7 @@ func (p *ProviderConfig) deleteRecords(deleted infobloxChangeMap) {
 						if p.dryRun {
 							logrus.Infof("Would delete %s record named '%s' to '%s' for Infoblox DNS zone '%s'.", "A", record.Name, record.Ipv4Addr, record.Zone)
 						} else {
-							logrus.Debugf("Deleting %s record named '%s' to '%s' for Infoblox DNS zone '%s'.", "A", record.Name, record.Ipv4Addr, record.Zone)
+							logrus.Infof("Deleting %s record named '%s' to '%s' for Infoblox DNS zone '%s'.", "A", record.Name, record.Ipv4Addr, record.Zone)
 							_, err = p.client.DeleteObject(record.Ref)
 						}
 					}
@@ -688,7 +688,7 @@ func (p *ProviderConfig) deleteRecords(deleted infobloxChangeMap) {
 						if p.dryRun {
 							logrus.Infof("Would delete %s record named '%s' to '%s' for Infoblox DNS zone '%s'.", "PTR", record.PtrdName, record.Ipv4Addr, record.Zone)
 						} else {
-							logrus.Debugf("Deleting %s record named '%s' to '%s' for Infoblox DNS zone '%s'.", "PTR", record.PtrdName, record.Ipv4Addr, record.Zone)
+							logrus.Infof("Deleting %s record named '%s' to '%s' for Infoblox DNS zone '%s'.", "PTR", record.PtrdName, record.Ipv4Addr, record.Zone)
 							_, err = p.client.DeleteObject(record.Ref)
 						}
 					}
@@ -697,7 +697,7 @@ func (p *ProviderConfig) deleteRecords(deleted infobloxChangeMap) {
 						if p.dryRun {
 							logrus.Infof("Would delete %s record named '%s' to '%s' for Infoblox DNS zone '%s'.", "CNAME", record.Name, record.Canonical, record.Zone)
 						} else {
-							logrus.Debugf("Deleting %s record named '%s' to '%s' for Infoblox DNS zone '%s'.", "CNAME", record.Name, record.Canonical, record.Zone)
+							logrus.Infof("Deleting %s record named '%s' to '%s' for Infoblox DNS zone '%s'.", "CNAME", record.Name, record.Canonical, record.Zone)
 							_, err = p.client.DeleteObject(record.Ref)
 						}
 					}
@@ -706,7 +706,7 @@ func (p *ProviderConfig) deleteRecords(deleted infobloxChangeMap) {
 						if p.dryRun {
 							logrus.Infof("Would delete %s record named '%s' to '%s' for Infoblox DNS zone '%s'.", "TXT", record.Name, record.Text, record.Zone)
 						} else {
-							logrus.Debugf("Deleting %s record named '%s' to '%s' for Infoblox DNS zone '%s'.", "TXT", record.Name, record.Text, record.Zone)
+							logrus.Infof("Deleting %s record named '%s' to '%s' for Infoblox DNS zone '%s'.", "TXT", record.Name, record.Text, record.Zone)
 							_, err = p.client.DeleteObject(record.Ref)
 						}
 					}
