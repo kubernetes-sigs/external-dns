41c52031d04b173c785fa5324a679c18642f6dec
Sergey Matyukevich <s.matyukevich@gmail.com>
2022-11-22 14:41:20 -0700

    Increase timeout in psyduck

--- source/source.go
+++ source/source.go
@@ -346,7 +346,7 @@ type informerFactory interface {
 }
 
 func waitForCacheSync(ctx context.Context, factory informerFactory) error {
-	ctx, cancel := context.WithTimeout(ctx, 60*time.Second)
+	ctx, cancel := context.WithTimeout(ctx, 180*time.Second)
 	defer cancel()
 	for typ, done := range factory.WaitForCacheSync(ctx.Done()) {
 		if !done {
