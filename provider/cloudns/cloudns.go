dd0c44673a9334f20b9d6bcac4bb1f0e31d3e533
William Marchesi <will@marchesi.io>
2022-12-20 01:05:11 -0500

    NewClouDNSProvider Func Implemented
    Signed-off-by: William Marchesi <will@marchesi.io>

--- provider/cloudns/cloudns.go
+++ provider/cloudns/cloudns.go
@@ -17,6 +17,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strconv"
 
 	cloudns "github.com/wmarchesi123/cloudns-go"
 
@@ -48,6 +49,8 @@ type ClouDNSConfig struct {
 
 func NewClouDNSProvider(config ClouDNSConfig) (*ClouDNSProvider, error) {
 
+	var client *cloudns.Client
+
 	log.Info("Creating ClouDNS Provider")
 
 	loginType, ok := os.LookupEnv("CLOUDNS_LOGIN_TYPE")
@@ -58,16 +61,86 @@ func NewClouDNSProvider(config ClouDNSConfig) (*ClouDNSProvider, error) {
 		return nil, fmt.Errorf("CLOUDNS_LOGIN_TYPE is not valid")
 	}
 
+	userPassword, ok := os.LookupEnv("CLOUDNS_USER_PASSWORD")
+	if !ok {
+		return nil, fmt.Errorf("CLOUDNS_USER_PASSWORD is not set")
+	}
+
 	switch loginType {
 	case "user-id":
 		log.Info("Using user-id login type")
+
+		userIDString, ok := os.LookupEnv("CLOUDNS_USER_ID")
+		if !ok {
+			return nil, fmt.Errorf("CLOUDNS_USER_ID is not set")
+		}
+
+		userIDInt, error := strconv.Atoi(userIDString)
+		if error != nil {
+			return nil, fmt.Errorf("CLOUDNS_USER_ID is not a valid integer")
+		}
+
+		c, error := cloudns.New(
+			cloudns.AuthUserID(userIDInt, userPassword),
+		)
+		if error != nil {
+			return nil, fmt.Errorf("error creating ClouDNS client: %s", error)
+		}
+
+		client = c
+		log.Info("Authenticated with ClouDNS using user-id login type")
 	case "sub-user":
 		log.Info("Using sub-user login type")
+
+		subUYserIDString, ok := os.LookupEnv("CLOUDNS_SUB_USER_ID")
+		if !ok {
+			return nil, fmt.Errorf("CLOUDNS_SUB_USER_ID is not set")
+		}
+
+		subUserIDInt, error := strconv.Atoi(subUYserIDString)
+		if error != nil {
+			return nil, fmt.Errorf("CLOUDNS_SUB_USER_ID is not a valid integer")
+		}
+
+		c, error := cloudns.New(
+			cloudns.AuthSubUserID(subUserIDInt, userPassword),
+		)
+		if error != nil {
+			return nil, fmt.Errorf("error creating ClouDNS client: %s", error)
+		}
+
+		client = c
+		log.Info("Authenticated with ClouDNS using sub-user login type")
 	case "sub-user-name":
 		log.Info("Using sub-user-name login type")
+
+		subUserName, ok := os.LookupEnv("CLOUDNS_SUB_USER_NAME")
+		if !ok {
+			return nil, fmt.Errorf("CLOUDNS_SUB_USER_NAME is not set")
+		}
+
+		c, error := cloudns.New(
+			cloudns.AuthSubUserName(subUserName, userPassword),
+		)
+		if error != nil {
+			return nil, fmt.Errorf("error creating ClouDNS client: %s", error)
+		}
+
+		client = c
+		log.Info("Authenticated with ClouDNS using sub-user-name login type")
 	}
 
-	return nil, nil
+	provider := &ClouDNSProvider{
+		client:       client,
+		context:      config.Context,
+		domainFilter: config.DomainFilter,
+		zoneIDFilter: config.ZoneIDFilter,
+		ownerID:      config.OwnerID,
+		dryRun:       config.DryRun,
+		testing:      config.Testing,
+	}
+
+	return provider, nil
 }
 
 func (p *ClouDNSProvider) Records(ctx context.Context) ([]*endpoint.Endpoint, error) {
