60350b62dbd90facf3d8d1aac9c2a0353b114d30
theloneexplorerquest <theloneexplorerquest@gmail.com>
2023-12-05 22:17:24 +1100

    update test for valid aesKey

--- endpoint/crypto_test.go
+++ endpoint/crypto_test.go
@@ -24,8 +24,11 @@ import (
 
 func TestEncrypt(t *testing.T) {
 	// Verify that text encryption and decryption works
-	aesKey := []byte("1WGwUWyQ-vnVIZNpAT8k3i2Vjm4_2xacplj_8FzF7K8=")
-	plaintext := "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+	//aesKey := []byte("1WGwUWyQ-vnVIZNpAT8k3i2Vjm4_2xacplj_8FzF7K8=")
+	aesKey := []byte("UHopdKnIiiLPQWo3DXVpU5crYdM7pV1nkzD0TJLqJ4M=")
+	//plaintext := "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+	plaintext := "heritage=external-dns,external-dns/owner=foo-owner,external-dns/resource=foo-resource"
+
 	encryptedtext, err := EncryptText(plaintext, aesKey, nil)
 	require.NoError(t, err)
 	decryptedtext, _, err := DecryptText(encryptedtext, aesKey)
