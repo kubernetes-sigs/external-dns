b716b764af6323a4affc65ed44623dbaf5ee3bde
Joel Studler <joel.studler@swisscom.com>
2024-01-26 18:27:28 +0100

    Fixing NAPTR support (https://github.com/kubernetes-sigs/external-dns/issues/3979)

--- source/crd.go
+++ source/crd.go
@@ -190,7 +190,10 @@ func (cs *crdSource) Endpoints(ctx context.Context) ([]*endpoint.Endpoint, error
 
 			illegalTarget := false
 			for _, target := range ep.Targets {
-				if strings.HasSuffix(target, ".") {
+				if ep.RecordType != "NAPTR" && strings.HasSuffix(target, ".") {
+					illegalTarget = true
+					break
+				} else if ep.RecordType == "NAPTR" && !strings.HasSuffix(target, ".") {
 					illegalTarget = true
 					break
 				}
