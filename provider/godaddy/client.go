7fb8be165ad5d1e2fcb7e706d4b6f9ef70c314e3
rayshoo <fire@dragonz.dev>
2024-11-14 12:47:54 +0900

    goDaddy API one month limited handling

--- provider/godaddy/client.go
+++ provider/godaddy/client.go
@@ -230,11 +230,15 @@ func (c *Client) Do(req *http.Request) (*http.Response, error) {
 	resp, err := c.Client.Do(req)
 	// In case of several clients behind NAT we still can hit rate limit
 	for i := 1; i < 3 && err == nil && resp.StatusCode == 429; i++ {
-		retryAfter, _ := strconv.ParseInt(resp.Header.Get("Retry-After"), 10, 0)
-
+		var retryAfter int64
 		var jitter int64
-		if retryAfter > 0 {
-			jitter = rand.Int63n(retryAfter)
+		if headerValue := resp.Header.Get("Retry-After"); headerValue == "" {
+			retryAfter = 30
+		} else {
+			retryAfter, _ = strconv.ParseInt(headerValue, 10, 0)
+			if retryAfter > 0 {
+				jitter = rand.Int63n(retryAfter)
+			}
 		}
 		retryAfterSec := retryAfter + jitter/2
 
