70b2141691921946853aa18a822722c554a5ad7a
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2025-01-28 19:02:50 +0000

    chore: tags filtering update test with not supported tag formats
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- pkg/filters/zonetagfilter/zone_tag_filter_test.go
+++ pkg/filters/zonetagfilter/zone_tag_filter_test.go
@@ -67,6 +67,24 @@ func TestZoneTagFilterMatch(t *testing.T) {
 	}
 }
 
+func TestZoneTagFilterNotSupportedFormat(t *testing.T) {
+	tests := []struct {
+		desc string
+		tags []string
+		want []string
+	}{
+		{desc: "multiple or separate values with commas", tags: []string{"key1=val1,key2=val2"}, want: []string{"key1=val1,key2=val2"}},
+		{desc: "exclude tag", tags: []string{"!key1"}, want: []string{"!key1"}},
+		{desc: "exclude tags", tags: []string{"!key1=val"}, want: []string{"!key1=val"}},
+	}
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("%s", tc.desc), func(t *testing.T) {
+			got := NewZoneTagFilter(tc.tags)
+			assert.Equal(t, tc.want, got.zoneTags)
+		})
+	}
+}
+
 func TestZoneTagFilterMatchGeneratedValues(t *testing.T) {
 	tests := []struct {
 		filters int
