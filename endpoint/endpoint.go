2481c07e95a2e6baff8d74d59c577dc3daf33186
Michel Loiseleur <97035654+mloiseleur@users.noreply.github.com>
2025-05-04 21:53:56 +0200

    chore(crd): use conventional paths and update controller-gen to v0.17.2 (#5287)
    * chore(crd): update controller-gen to v0.17.2
    
    * review: only one crd manifest
    
    * set crd in expected paths

--- endpoint/endpoint.go
+++ endpoint/endpoint.go
@@ -204,6 +204,7 @@ type EndpointKey struct {
 }
 
 // Endpoint is a high-level way of a connection between a service and an IP
+// +kubebuilder:object:generate=true
 type Endpoint struct {
 	// The hostname of the DNS record
 	DNSName string `json:"dnsName,omitempty"`
@@ -337,6 +338,7 @@ func FilterEndpointsByOwnerID(ownerID string, eps []*Endpoint) []*Endpoint {
 }
 
 // DNSEndpointSpec defines the desired state of DNSEndpoint
+// +kubebuilder:object:generate=true
 type DNSEndpointSpec struct {
 	Endpoints []*Endpoint `json:"endpoints,omitempty"`
 }
