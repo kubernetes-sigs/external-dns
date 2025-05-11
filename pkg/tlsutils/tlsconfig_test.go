b8808670dba145650afdae975d7c71ebce41f779
Lino Layani <layanilino@gmail.com>
2025-05-11 11:43:02 -0400

    Add licence banner

--- pkg/tlsutils/tlsconfig_test.go
+++ pkg/tlsutils/tlsconfig_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2023 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package tlsutils
 
 import (
@@ -74,7 +90,7 @@ func TestCreateTLSConfig(t *testing.T) {
 			},
 		},
 		{
-			"Valid cert and key return a valid tls.Config with certificates",
+			"Valid cert and key return a valid tls.Config with a certificate",
 			"prefix",
 			"",
 			rsaCertPEM,
@@ -84,7 +100,7 @@ func TestCreateTLSConfig(t *testing.T) {
 			func(actual *tls.Config, err error) {
 				assert.Nil(t, err)
 				assert.Equal(t, actual.ServerName, "server-name")
-				assert.NotNil(t, actual.Certificates)
+				assert.NotNil(t, actual.Certificates[0])
 				assert.Equal(t, actual.InsecureSkipVerify, false)
 				assert.Equal(t, actual.MinVersion, uint16(defaultMinVersion))
 			},
