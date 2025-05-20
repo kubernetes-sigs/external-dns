8fbcd6a234b1dc49bafb96d8aa7f4653575c1235
yuzeng.zyx <yuzeng.zyx@alibaba-inc.com>
2025-05-20 16:50:32 +0800

    fix lint

--- provider/alibabacloud/alibaba_cloud.go
+++ provider/alibabacloud/alibaba_cloud.go
@@ -498,19 +498,18 @@ func (p *AlibabaCloudProvider) unescapeTXTRecordValue(value string) string {
 }
 
 func (p *AlibabaCloudProvider) createRecord(endpoint *endpoint.Endpoint, target string, hostedZoneDomains []string) error {
-
 	if len(hostedZoneDomains) == 0 {
-		log.Errorf("Failed to create %s record named '%s' to '%s' for Alibaba Cloud DNS: not found any zone,please add the DNS zone first",
+		log.Errorf("Failed to create %s record named '%s' to '%s' for Alibaba Cloud DNS: zone not found",
 			endpoint.RecordType, endpoint.DNSName, target)
-		return fmt.Errorf("no found any zone,please add the DNS zone first")
+		return fmt.Errorf("zone not found")
 	}
 
 	rr, domain := p.splitDNSName(endpoint.DNSName, hostedZoneDomains)
 
 	if domain == "" {
-		log.Errorf("Failed to create %s record named '%s' to '%s' for Alibaba Cloud DNS: no coorsponing DNS zone found for this domain '%s',please add the DNS zone first",
+		log.Errorf("Failed to create %s record named '%s' to '%s' for Alibaba Cloud DNS: no corresponding DNS zone found for this domain '%s'",
 			endpoint.RecordType, endpoint.DNSName, target, endpoint.DNSName)
-		return fmt.Errorf("no corresponding DNS zone found for this domain. Please add the DNS zone first")
+		return fmt.Errorf("no corresponding DNS zone found for this domain")
 	}
 
 	request := alidns.CreateAddDomainRecordRequest()
