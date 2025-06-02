b509aec5151272a476cba5c565563c73d0bb61b3
Tom M G <tmg.7@icloud.com>
2025-06-02 14:28:43 +0200

    Set tags via providerSpecific annotations

--- source/annotations/provider_specific.go
+++ source/annotations/provider_specific.go
@@ -73,6 +73,12 @@ func ProviderSpecificAnnotations(annotations map[string]string) (endpoint.Provid
 					Name:  CloudflareRecordCommentKey,
 					Value: v,
 				})
+
+			} else if strings.Contains(k, CloudflareRecordTagsKey) {
+				providerSpecificAnnotations = append(providerSpecificAnnotations, endpoint.ProviderSpecificProperty{
+					Name:  CloudflareRecordTagsKey,
+					Value: v,
+				})
 			}
 		}
 	}
