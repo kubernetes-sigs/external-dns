d3b6abd7f62704d81f8abe9d71c25e76e6cc8974
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2025-10-11 12:29:34 +0100

    endpoints benchmarking
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- endpoint/endpoint_bechmark_test.go
+++ endpoint/endpoint_bechmark_test.go
@@ -152,16 +152,16 @@ func BenchmarkEndpoint100With10PropertiesHashMap(b *testing.B) {
 
 	for b.Loop() {
 		for _, e := range endpoints {
-			e.SetProviderSpecificProperty("prop1", "value1")
-			e.SetProviderSpecificProperty("prop2", "value2")
-			e.SetProviderSpecificProperty("prop3", "value3")
-			e.SetProviderSpecificProperty("prop4", "value3")
-			e.SetProviderSpecificProperty("prop5", "value3")
-			e.SetProviderSpecificProperty("prop6", "value3")
-			e.SetProviderSpecificProperty("prop7", "value3")
-			e.SetProviderSpecificProperty("prop8", "value3")
-			e.SetProviderSpecificProperty("prop9", "value3")
-			e.SetProviderSpecificProperty("prop10", "value3")
+			e.SetProviderSpecificPropertyHashMap("prop1", "value1")
+			e.SetProviderSpecificPropertyHashMap("prop2", "value2")
+			e.SetProviderSpecificPropertyHashMap("prop3", "value3")
+			e.SetProviderSpecificPropertyHashMap("prop4", "value3")
+			e.SetProviderSpecificPropertyHashMap("prop5", "value3")
+			e.SetProviderSpecificPropertyHashMap("prop6", "value3")
+			e.SetProviderSpecificPropertyHashMap("prop7", "value3")
+			e.SetProviderSpecificPropertyHashMap("prop8", "value3")
+			e.SetProviderSpecificPropertyHashMap("prop9", "value3")
+			e.SetProviderSpecificPropertyHashMap("prop10", "value3")
 		}
 	}
 }
@@ -171,16 +171,16 @@ func BenchmarkEndpoint1000With10PropertiesHashMap(b *testing.B) {
 
 	for b.Loop() {
 		for _, e := range endpoints {
-			e.SetProviderSpecificProperty("prop1", "value1")
-			e.SetProviderSpecificProperty("prop2", "value2")
-			e.SetProviderSpecificProperty("prop3", "value3")
-			e.SetProviderSpecificProperty("prop4", "value3")
-			e.SetProviderSpecificProperty("prop5", "value3")
-			e.SetProviderSpecificProperty("prop6", "value3")
-			e.SetProviderSpecificProperty("prop7", "value3")
-			e.SetProviderSpecificProperty("prop8", "value3")
-			e.SetProviderSpecificProperty("prop9", "value3")
-			e.SetProviderSpecificProperty("prop10", "value3")
+			e.SetProviderSpecificPropertyHashMap("prop1", "value1")
+			e.SetProviderSpecificPropertyHashMap("prop2", "value2")
+			e.SetProviderSpecificPropertyHashMap("prop3", "value3")
+			e.SetProviderSpecificPropertyHashMap("prop4", "value3")
+			e.SetProviderSpecificPropertyHashMap("prop5", "value3")
+			e.SetProviderSpecificPropertyHashMap("prop6", "value3")
+			e.SetProviderSpecificPropertyHashMap("prop7", "value3")
+			e.SetProviderSpecificPropertyHashMap("prop8", "value3")
+			e.SetProviderSpecificPropertyHashMap("prop9", "value3")
+			e.SetProviderSpecificPropertyHashMap("prop10", "value3")
 		}
 	}
 }
