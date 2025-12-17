f87bab015c9ceebc2a92ca17dc04ad0f0849adb0
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2025-12-17 22:29:36 +0000

    feat(cli): rollbak to kingpin
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- pkg/apis/externaldns/binders.go
+++ pkg/apis/externaldns/binders.go
@@ -23,7 +23,6 @@ import (
 	"time"
 
 	"github.com/alecthomas/kingpin/v2"
-	"github.com/spf13/cobra"
 )
 
 // FlagBinder abstracts flag registration for different CLI backends.
@@ -142,60 +141,4 @@ func setRegexpDefault(rs regexpSetter, def *regexp.Regexp, name string) {
 			panic(fmt.Errorf("invalid default regexp for flag %s: %w", name, err))
 		}
 	}
-}
-
-// CobraBinder implements FlagBinder using github.com/spf13/cobra.
-type CobraBinder struct {
-	Cmd *cobra.Command
-}
-
-// NewCobraBinder creates a FlagBinder backed by a Cobra command.
-func NewCobraBinder(cmd *cobra.Command) *CobraBinder {
-	return &CobraBinder{Cmd: cmd}
-}
-
-func (b *CobraBinder) StringVar(name, help, def string, target *string) {
-	b.Cmd.Flags().StringVar(target, name, def, help)
-}
-
-func (b *CobraBinder) BoolVar(name, help string, def bool, target *bool) {
-	b.Cmd.Flags().BoolVar(target, name, def, help)
-}
-
-func (b *CobraBinder) DurationVar(name, help string, def time.Duration, target *time.Duration) {
-	b.Cmd.Flags().DurationVar(target, name, def, help)
-}
-
-func (b *CobraBinder) IntVar(name, help string, def int, target *int) {
-	b.Cmd.Flags().IntVar(target, name, def, help)
-}
-
-func (b *CobraBinder) Int64Var(name, help string, def int64, target *int64) {
-	b.Cmd.Flags().Int64Var(target, name, def, help)
-}
-
-func (b *CobraBinder) StringsVar(name, help string, def []string, target *[]string) {
-	// Preserve repeatable flag semantics.
-	b.Cmd.Flags().StringArrayVar(target, name, def, help)
-}
-
-func (b *CobraBinder) EnumVar(name, help, def string, target *string, allowed ...string) {
-	b.Cmd.Flags().StringVar(target, name, def, help)
-}
-
-func (b *CobraBinder) StringsEnumVar(name, help string, def []string, target *[]string, allowed ...string) {
-	// pflag does not enforce enums.
-	b.Cmd.Flags().StringArrayVar(target, name, def, help)
-}
-
-func (b *CobraBinder) StringMapVar(name, help string, target *map[string]string) {
-	// Use StringToStringVar for key=value pairs.
-	b.Cmd.Flags().StringToStringVar(target, name, map[string]string{}, help)
-}
-
-func (b *CobraBinder) RegexpVar(name, help string, def *regexp.Regexp, target **regexp.Regexp) {
-	rv := &regexpValue{target: target}
-	// set default value to mirror kingpin's Default(def.String()) behavior
-	setRegexpDefault(rv, def, name)
-	b.Cmd.Flags().Var(rv, name, help)
 }
--- pkg/apis/externaldns/binders_test.go
+++ pkg/apis/externaldns/binders_test.go
@@ -23,7 +23,6 @@ import (
 	"time"
 
 	"github.com/alecthomas/kingpin/v2"
-	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -96,67 +95,6 @@ func TestKingpinBinderStringsVarNoDefaultAndBoolDefaultFalse(t *testing.T) {
 	assert.False(t, b2)
 }
 
-func TestCobraBinderParsesAllTypes(t *testing.T) {
-	cmd := &cobra.Command{Use: "test"}
-	b := NewCobraBinder(cmd)
-
-	var (
-		s    string
-		bval bool
-		d    time.Duration
-		i    int
-		i64  int64
-		ss   []string
-		e    string
-	)
-
-	b.StringVar("s", "string flag", "def", &s)
-	b.BoolVar("b", "bool flag", true, &bval)
-	b.DurationVar("d", "duration flag", 5*time.Second, &d)
-	b.IntVar("i", "int flag", 7, &i)
-	b.Int64Var("i64", "int64 flag", 9, &i64)
-	b.StringsVar("ss", "strings flag", []string{"x"}, &ss)
-	b.EnumVar("e", "enum flag", "a", &e, "a", "b")
-
-	cmd.SetArgs([]string{"--s=abc", "--b=false", "--d=2s", "--i=42", "--i64=64", "--ss=one", "--ss=two", "--e=b"})
-	err := cmd.Execute()
-	require.NoError(t, err)
-
-	assert.Equal(t, "abc", s)
-	assert.False(t, bval)
-	assert.Equal(t, 2*time.Second, d)
-	assert.Equal(t, 42, i)
-	assert.Equal(t, int64(64), i64)
-	assert.ElementsMatch(t, []string{"one", "two"}, ss)
-	assert.Equal(t, "b", e)
-}
-
-func TestCobraBinderEnumNotValidatedHere(t *testing.T) {
-	cmd := &cobra.Command{Use: "test"}
-	b := NewCobraBinder(cmd)
-
-	var e string
-	b.EnumVar("e", "enum flag", "a", &e, "a", "b")
-
-	cmd.SetArgs([]string{"--e=c"})
-	err := cmd.Execute()
-	require.NoError(t, err)
-	assert.Equal(t, "c", e)
-}
-
-// Cobra requires --<flag>=false
-func TestCobraBinderNoBooleanNegationFormUnsupported(t *testing.T) {
-	cmd := &cobra.Command{Use: "test"}
-	b := NewCobraBinder(cmd)
-
-	var v bool
-	b.BoolVar("v", "bool flag", true, &v)
-
-	cmd.SetArgs([]string{"--no-v"})
-	err := cmd.Execute()
-	require.Error(t, err)
-}
-
 func TestCobraRegexValueSetStringType(t *testing.T) {
 	var r *regexp.Regexp
 	rv := &regexpValue{target: &r}
@@ -175,40 +113,6 @@ func TestCobraRegexValueSetStringType(t *testing.T) {
 	require.NotNil(t, r)
 	assert.Equal(t, "^foo$", r.String())
 	assert.Equal(t, "^foo$", rv.String())
-}
-
-func TestCobraRegexpVarDefaultAndInvalidValue(t *testing.T) {
-	cmd := &cobra.Command{Use: "test"}
-	b := NewCobraBinder(cmd)
-
-	var r *regexp.Regexp
-	// Provide a valid default: should set target immediately
-	b.RegexpVar("re", "help", regexp.MustCompile("^x+$"), &r)
-	require.NotNil(t, r)
-	assert.Equal(t, "^x+$", r.String())
-
-	// Executing with an invalid value should produce an error
-	cmd2 := &cobra.Command{Use: "test2"}
-	b2 := NewCobraBinder(cmd2)
-	var r2 *regexp.Regexp
-	b2.RegexpVar("re", "help", nil, &r2)
-	cmd2.SetArgs([]string{"--re=invalid("})
-	err := cmd2.Execute()
-	require.Error(t, err)
-}
-
-func TestCobraStringMapVarDefaultEmpty(t *testing.T) {
-	cmd := &cobra.Command{Use: "test"}
-	b := NewCobraBinder(cmd)
-
-	var m map[string]string
-	b.StringMapVar("m", "help", &m)
-
-	cmd.SetArgs([]string{})
-	err := cmd.Execute()
-	require.NoError(t, err)
-	require.NotNil(t, m)
-	assert.Empty(t, m)
 }
 
 func TestKingpinRegexpVarDefaultAndParse(t *testing.T) {
@@ -252,26 +156,6 @@ func TestKingpinStringsEnumVarWithAndWithoutDefault(t *testing.T) {
 	_, err = app2.Parse([]string{"--se=a", "--se=c"})
 	require.NoError(t, err)
 	assert.ElementsMatch(t, []string{"a", "c"}, vals2)
-}
-
-func TestCobraStringsEnumVarWithAndWithoutDefault(t *testing.T) {
-	cmd := &cobra.Command{Use: "test"}
-	b := NewCobraBinder(cmd)
-
-	var vals []string
-	b.StringsEnumVar("se", "help", []string{"x", "y"}, &vals, "x", "y")
-	cmd.SetArgs([]string{})
-	require.NoError(t, cmd.Execute())
-	assert.ElementsMatch(t, []string{"x", "y"}, vals)
-
-	// without default
-	cmd2 := &cobra.Command{Use: "test2"}
-	b2 := NewCobraBinder(cmd2)
-	var vals2 []string
-	b2.StringsEnumVar("se", "help", nil, &vals2, "x", "y")
-	cmd2.SetArgs([]string{"--se=a", "--se=b"})
-	require.NoError(t, cmd2.Execute())
-	assert.ElementsMatch(t, []string{"a", "b"}, vals2)
 }
 
 func TestSetRegexDefaultPanicsOnInvalidDefault(t *testing.T) {
