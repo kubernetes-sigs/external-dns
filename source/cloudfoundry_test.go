0c6b8caf890d25e9c52558f9311dd452b26c4e79
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2025-12-20 17:46:22 +0000

    test(coverage): improve code coverage for different files
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- source/cloudfoundry_test.go
+++ source/cloudfoundry_test.go
@@ -17,10 +17,17 @@ limitations under the License.
 package source
 
 import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/cloudfoundry-community/go-cfclient"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+
+	"sigs.k8s.io/external-dns/endpoint"
 )
 
 type RouteSuite struct {
@@ -35,4 +42,112 @@ func TestRouteSource(t *testing.T) {
 // testRouteSourceImplementsSource tests that cloudfoundrySource is a valid Source.
 func testRouteSourceImplementsSource(t *testing.T) {
 	require.Implements(t, (*Source)(nil), new(cloudfoundrySource))
+}
+
+func TestCloudFoundrySourceEndpoints(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.Path {
+		case "/v2/private_domains":
+			w.Header().Set("Content-Type", "application/json")
+			if err := json.NewEncoder(w).Encode(map[string]interface{}{
+				"total_results": 1,
+				"total_pages":   1,
+				"next_url":      "",
+				"resources": []map[string]interface{}{
+					{
+						"metadata": map[string]interface{}{
+							"guid":       "domain-guid",
+							"created_at": "now",
+							"updated_at": "now",
+						},
+						"entity": map[string]interface{}{
+							"name": "example.com",
+						},
+					},
+				},
+			}); err != nil {
+				t.Errorf("encode domains response: %v", err)
+			}
+		case "/v2/routes":
+			if got := r.URL.Query().Get("q"); got != "domain_guid:domain-guid" {
+				t.Errorf("expected domain query to be %q, got %q", "domain_guid:domain-guid", got)
+			}
+			w.Header().Set("Content-Type", "application/json")
+			if err := json.NewEncoder(w).Encode(map[string]interface{}{
+				"total_results": 2,
+				"total_pages":   1,
+				"next_url":      "",
+				"resources": []map[string]interface{}{
+					{
+						"metadata": map[string]interface{}{
+							"guid":       "route-guid",
+							"created_at": "now",
+							"updated_at": "now",
+						},
+						"entity": map[string]interface{}{
+							"host":        "app",
+							"domain_guid": "domain-guid",
+						},
+					},
+					{
+						"metadata": map[string]interface{}{
+							"guid":       "route-guid-2",
+							"created_at": "now",
+							"updated_at": "now",
+						},
+						"entity": map[string]interface{}{
+							"host":        "api",
+							"domain_guid": "domain-guid",
+						},
+					},
+				},
+			}); err != nil {
+				t.Errorf("encode routes response: %v", err)
+			}
+		default:
+			http.NotFound(w, r)
+		}
+	}))
+	t.Cleanup(server.Close)
+
+	client := &cfclient.Client{
+		Config: cfclient.Config{
+			ApiAddress: server.URL,
+			HttpClient: server.Client(),
+			UserAgent:  "cloudfoundry-test",
+		},
+	}
+
+	source, err := NewCloudFoundrySource(client)
+	require.NoError(t, err)
+
+	source.AddEventHandler(context.Background(), func() {})
+	endpoints, err := source.Endpoints(context.Background())
+	require.NoError(t, err)
+
+	expected := []*endpoint.Endpoint{
+		endpoint.NewEndpointWithTTL("app.example.com", endpoint.RecordTypeCNAME, 300, "example.com"),
+		endpoint.NewEndpointWithTTL("api.example.com", endpoint.RecordTypeCNAME, 300, "example.com"),
+	}
+	for _, ep := range expected {
+		ep.Targets[0] = server.Listener.Addr().String()
+	}
+	require.ElementsMatch(t, expected, endpoints)
+}
+
+func TestCloudFoundrySourceEndpointsPanicsOnBadURL(t *testing.T) {
+	client := &cfclient.Client{
+		Config: cfclient.Config{
+			ApiAddress: "http://[::1]:namedport",
+			HttpClient: http.DefaultClient,
+			UserAgent:  "cloudfoundry-test",
+		},
+	}
+
+	source, err := NewCloudFoundrySource(client)
+	require.NoError(t, err)
+
+	require.Panics(t, func() {
+		_, _ = source.Endpoints(context.Background())
+	})
 }
