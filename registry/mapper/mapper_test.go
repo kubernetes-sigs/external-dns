47ee72ad10ec20517baa7355822e503de70e76fd
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2025-12-30 14:32:49 +0000

    chore(registry): move registries to its own packages
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- registry/mapper/mapper_test.go
+++ registry/mapper/mapper_test.go
@@ -20,6 +20,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"sigs.k8s.io/external-dns/endpoint"
 )
 
 var (
@@ -35,39 +36,123 @@ func TestAffixNameMapper_ToEndpointName(t *testing.T) {
 		wantRecordType   string
 	}{
 		{
-			name:             "prefix with record type in affix",
+			name:             "prefix with A record type in affix",
 			mapper:           NewAffixNameMapper("%{record_type}-", "", ""),
 			input:            "a-foo.example.com",
 			wantEndpointName: "foo.example.com",
-			wantRecordType:   "A",
+			wantRecordType:   endpoint.RecordTypeA,
 		},
 		{
-			name:             "suffix with record type in affix",
+			name:             "prefix with AAAA record type in affix",
+			mapper:           NewAffixNameMapper("%{record_type}-", "", ""),
+			input:            "aaaa-foo.example.com",
+			wantEndpointName: "foo.example.com",
+			wantRecordType:   endpoint.RecordTypeAAAA,
+		},
+		{
+			name:             "prefix with CNAME record type in affix",
+			mapper:           NewAffixNameMapper("%{record_type}-", "", ""),
+			input:            "cname-foo.example.com",
+			wantEndpointName: "foo.example.com",
+			wantRecordType:   endpoint.RecordTypeCNAME,
+		},
+		{
+			name:             "prefix with NS record type in affix",
+			mapper:           NewAffixNameMapper("%{record_type}-", "", ""),
+			input:            "ns-foo.example.com",
+			wantEndpointName: "foo.example.com",
+			wantRecordType:   endpoint.RecordTypeNS,
+		},
+		{
+			name:             "prefix with MX record type in affix",
+			mapper:           NewAffixNameMapper("%{record_type}-", "", ""),
+			input:            "mx-foo.example.com",
+			wantEndpointName: "foo.example.com",
+			wantRecordType:   endpoint.RecordTypeMX,
+		},
+		{
+			name:             "prefix with SRV record type in affix",
+			mapper:           NewAffixNameMapper("%{record_type}-", "", ""),
+			input:            "srv-foo.example.com",
+			wantEndpointName: "foo.example.com",
+			wantRecordType:   endpoint.RecordTypeSRV,
+		},
+		{
+			name:             "prefix with NAPTR record type in affix",
+			mapper:           NewAffixNameMapper("%{record_type}-", "", ""),
+			input:            "naptr-foo.example.com",
+			wantEndpointName: "foo.example.com",
+			wantRecordType:   endpoint.RecordTypeNAPTR,
+		},
+		{
+			name:             "suffix with A record type in affix",
 			mapper:           NewAffixNameMapper("", "-%{record_type}", ""),
-			input:            "foo.example.com-a",
+			input:            "foo-a.example.com",
 			wantEndpointName: "foo.example.com",
-			wantRecordType:   "A",
+			wantRecordType:   endpoint.RecordTypeA,
 		},
 		{
-			name:             "prefix without record type in affix",
-			mapper:           NewAffixNameMapper("txt-", "", ""),
-			input:            "a-foo.example.com",
+			name:             "suffix with CNAME record type in affix",
+			mapper:           NewAffixNameMapper("", "-%{record_type}", ""),
+			input:            "foo-cname.example.com",
 			wantEndpointName: "foo.example.com",
-			wantRecordType:   "A",
+			wantRecordType:   endpoint.RecordTypeCNAME,
 		},
 		{
-			name:             "suffix without record type in affix",
-			mapper:           NewAffixNameMapper("", "-txt", ""),
-			input:            "foo.example.com-a",
-			wantEndpointName: "foo.example.com",
-			wantRecordType:   "A",
-		},
-		{
-			name:             "no affix",
+			name:             "no affix with A record",
 			mapper:           NewAffixNameMapper("", "", ""),
 			input:            "a-foo.example.com",
 			wantEndpointName: "foo.example.com",
-			wantRecordType:   "A",
+			wantRecordType:   endpoint.RecordTypeA,
+		},
+		{
+			name:             "no affix with AAAA record",
+			mapper:           NewAffixNameMapper("", "", ""),
+			input:            "aaaa-foo.example.com",
+			wantEndpointName: "foo.example.com",
+			wantRecordType:   endpoint.RecordTypeAAAA,
+		},
+		{
+			name:             "no affix with CNAME record",
+			mapper:           NewAffixNameMapper("", "", ""),
+			input:            "cname-foo.example.com",
+			wantEndpointName: "foo.example.com",
+			wantRecordType:   endpoint.RecordTypeCNAME,
+		},
+		{
+			name:             "no affix with NS record",
+			mapper:           NewAffixNameMapper("", "", ""),
+			input:            "ns-foo.example.com",
+			wantEndpointName: "foo.example.com",
+			wantRecordType:   endpoint.RecordTypeNS,
+		},
+		{
+			name:             "no affix with MX record",
+			mapper:           NewAffixNameMapper("", "", ""),
+			input:            "mx-foo.example.com",
+			wantEndpointName: "foo.example.com",
+			wantRecordType:   endpoint.RecordTypeMX,
+		},
+		{
+			name:             "no affix with SRV record",
+			mapper:           NewAffixNameMapper("", "", ""),
+			input:            "srv-foo.example.com",
+			wantEndpointName: "foo.example.com",
+			wantRecordType:   endpoint.RecordTypeSRV,
+		},
+		{
+			name:             "no affix with NAPTR record",
+			mapper:           NewAffixNameMapper("", "", ""),
+			input:            "naptr-foo.example.com",
+			wantEndpointName: "foo.example.com",
+			wantRecordType:   endpoint.RecordTypeNAPTR,
+		},
+		{
+			name:             "suffix with txt record",
+			mapper:           NewAffixNameMapper("", "", ""),
+			input:            "txt-foo.example.com",
+			wantEndpointName: "txt-foo.example.com",
+			wantRecordType:   "",
 		},
 	}
 
@@ -77,6 +162,18 @@ func TestAffixNameMapper_ToEndpointName(t *testing.T) {
 			assert.Equal(t, tt.wantEndpointName, gotName)
 			assert.Equal(t, tt.wantRecordType, gotType)
 		})
+	}
+
+	// Verify all supported records are tested
+	testedRecords := make(map[string]bool)
+	for _, tt := range tests {
+		if tt.wantRecordType != "" {
+			testedRecords[tt.wantRecordType] = true
+		}
+	}
+
+	for _, recordType := range supportedRecords {
+		assert.True(t, testedRecords[recordType], "Record type %s is in supportedRecords but not tested in TestAffixNameMapper_ToEndpointName", recordType)
 	}
 }
 
@@ -89,32 +186,130 @@ func TestAffixNameMapper_ToTXTName(t *testing.T) {
 		wantTXTName string
 	}{
 		{
-			name:        "prefix with record type in affix",
+			name:        "prefix with A record type in affix",
 			mapper:      NewAffixNameMapper("%{record_type}-", "", ""),
 			dns:         "foo.example.com",
-			recordType:  "A",
+			recordType:  endpoint.RecordTypeA,
 			wantTXTName: "a-foo.example.com",
 		},
 		{
-			name:        "suffix with record type in affix",
+			name:        "prefix with AAAA record type in affix",
+			mapper:      NewAffixNameMapper("%{record_type}-", "", ""),
+			dns:         "foo.example.com",
+			recordType:  endpoint.RecordTypeAAAA,
+			wantTXTName: "aaaa-foo.example.com",
+		},
+		{
+			name:        "prefix with CNAME record type in affix",
+			mapper:      NewAffixNameMapper("%{record_type}-", "", ""),
+			dns:         "foo.example.com",
+			recordType:  endpoint.RecordTypeCNAME,
+			wantTXTName: "cname-foo.example.com",
+		},
+		{
+			name:        "prefix with NS record type in affix",
+			mapper:      NewAffixNameMapper("%{record_type}-", "", ""),
+			dns:         "foo.example.com",
+			recordType:  endpoint.RecordTypeNS,
+			wantTXTName: "ns-foo.example.com",
+		},
+		{
+			name:        "prefix with MX record type in affix",
+			mapper:      NewAffixNameMapper("%{record_type}-", "", ""),
+			dns:         "foo.example.com",
+			recordType:  endpoint.RecordTypeMX,
+			wantTXTName: "mx-foo.example.com",
+		},
+		{
+			name:        "prefix with SRV record type in affix",
+			mapper:      NewAffixNameMapper("%{record_type}-", "", ""),
+			dns:         "foo.example.com",
+			recordType:  endpoint.RecordTypeSRV,
+			wantTXTName: "srv-foo.example.com",
+		},
+		{
+			name:        "prefix with NAPTR record type in affix",
+			mapper:      NewAffixNameMapper("%{record_type}-", "", ""),
+			dns:         "foo.example.com",
+			recordType:  endpoint.RecordTypeNAPTR,
+			wantTXTName: "naptr-foo.example.com",
+		},
+		{
+			name:        "suffix with A record type in affix",
 			mapper:      NewAffixNameMapper("", "-%{record_type}", ""),
 			dns:         "foo.example.com",
-			recordType:  "A",
+			recordType:  endpoint.RecordTypeA,
 			wantTXTName: "foo-a.example.com",
 		},
 		{
-			name:        "wildcard replacement",
+			name:        "suffix with CNAME record type in affix",
+			mapper:      NewAffixNameMapper("", "-%{record_type}", ""),
+			dns:         "foo.example.com",
+			recordType:  endpoint.RecordTypeCNAME,
+			wantTXTName: "foo-cname.example.com",
+		},
+		{
+			name:        "wildcard replacement with A record",
 			mapper:      NewAffixNameMapper("txt-", "", "wild"),
 			dns:         "*.example.com",
-			recordType:  "A",
+			recordType:  endpoint.RecordTypeA,
 			wantTXTName: "txt-a-wild.example.com",
 		},
 		{
-			name:        "no affix",
+			name:        "wildcard replacement with MX record",
+			mapper:      NewAffixNameMapper("txt-", "", "wild"),
+			dns:         "*.example.com",
+			recordType:  endpoint.RecordTypeMX,
+			wantTXTName: "txt-mx-wild.example.com",
+		},
+		{
+			name:        "no affix with A record",
 			mapper:      NewAffixNameMapper("", "", ""),
 			dns:         "foo.example.com",
-			recordType:  "A",
+			recordType:  endpoint.RecordTypeA,
 			wantTXTName: "a-foo.example.com",
+		},
+		{
+			name:        "no affix with AAAA record",
+			mapper:      NewAffixNameMapper("", "", ""),
+			dns:         "foo.example.com",
+			recordType:  endpoint.RecordTypeAAAA,
+			wantTXTName: "aaaa-foo.example.com",
+		},
+		{
+			name:        "no affix with CNAME record",
+			mapper:      NewAffixNameMapper("", "", ""),
+			dns:         "foo.example.com",
+			recordType:  endpoint.RecordTypeCNAME,
+			wantTXTName: "cname-foo.example.com",
+		},
+		{
+			name:        "no affix with NS record",
+			mapper:      NewAffixNameMapper("", "", ""),
+			dns:         "foo.example.com",
+			recordType:  endpoint.RecordTypeNS,
+			wantTXTName: "ns-foo.example.com",
+		},
+		{
+			name:        "no affix with MX record",
+			mapper:      NewAffixNameMapper("", "", ""),
+			dns:         "foo.example.com",
+			recordType:  endpoint.RecordTypeMX,
+			wantTXTName: "mx-foo.example.com",
+		},
+		{
+			name:        "no affix with SRV record",
+			mapper:      NewAffixNameMapper("", "", ""),
+			dns:         "foo.example.com",
+			recordType:  endpoint.RecordTypeSRV,
+			wantTXTName: "srv-foo.example.com",
+		},
+		{
+			name:        "no affix with NAPTR record",
+			mapper:      NewAffixNameMapper("", "", ""),
+			dns:         "foo.example.com",
+			recordType:  endpoint.RecordTypeNAPTR,
+			wantTXTName: "naptr-foo.example.com",
 		},
 	}
 
@@ -123,6 +318,16 @@ func TestAffixNameMapper_ToTXTName(t *testing.T) {
 			got := tt.mapper.ToTXTName(tt.dns, tt.recordType)
 			assert.Equal(t, tt.wantTXTName, got)
 		})
+	}
+
+	// Verify all supported records are tested
+	testedRecords := make(map[string]bool)
+	for _, tt := range tests {
+		testedRecords[tt.recordType] = true
+	}
+
+	for _, recordType := range supportedRecords {
+		assert.True(t, testedRecords[recordType], "Record type %s is in supportedRecords but not tested in TestAffixNameMapper_ToTXTName", recordType)
 	}
 }
 
