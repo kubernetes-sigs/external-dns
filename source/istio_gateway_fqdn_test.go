d549cf9a0a4795bd2f055c9c4d115519076cf25c
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2026-01-16 08:49:24 +0000

    chore(source): standardize sources with config
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- source/istio_gateway_fqdn_test.go
+++ source/istio_gateway_fqdn_test.go
@@ -62,11 +62,13 @@ func TestIstioGatewaySourceNewSourceWithFqdn(t *testing.T) {
 				t.Context(),
 				fake.NewClientset(),
 				istiofake.NewSimpleClientset(),
-				"",
-				tt.annotationFilter,
-				tt.fqdnTemplate,
-				false,
-				false,
+				&Config{
+					Namespace:                "",
+					AnnotationFilter:         tt.annotationFilter,
+					FQDNTemplate:             tt.fqdnTemplate,
+					CombineFQDNAndAnnotation: false,
+					IgnoreHostnameAnnotation: false,
+				},
 			)
 
 			if tt.expectError {
@@ -502,11 +504,13 @@ func TestIstioGatewaySourceFqdnTemplatingExamples(t *testing.T) {
 				t.Context(),
 				kubeClient,
 				istioClient,
-				"",
-				"",
-				tt.fqdnTemplate,
-				!tt.combineFqdn,
-				false,
+				&Config{
+					Namespace:                "",
+					AnnotationFilter:         "",
+					FQDNTemplate:             tt.fqdnTemplate,
+					CombineFQDNAndAnnotation: !tt.combineFqdn,
+					IgnoreHostnameAnnotation: false,
+				},
 			)
 			require.NoError(t, err)
 
