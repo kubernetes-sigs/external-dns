594170f9f96fa82e56355f68ca3f6d8fd4b15ba0
Andrew Hay <andrew.hay@benchmarkanalytics.com>
2026-01-21 16:55:56 -0500

    Revert "fix(ci): harden kubectl download in e2e-test.sh to prevent flakiness"
    This reverts commit 68e27e3aabca5b80907b40786815f329de3baa4f.

--- scripts/e2e-test.sh
+++ scripts/e2e-test.sh
@@ -20,9 +20,7 @@ kind create cluster
 
 # Install kubectl
 echo "Installing kubectl..."
-KUBECTL_VERSION=$(curl -L -s https://dl.k8s.io/release/stable.txt)
-echo "Downloading kubectl version: ${KUBECTL_VERSION}"
-curl -LO "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl
 sudo mv kubectl /usr/local/bin/kubectl
 
