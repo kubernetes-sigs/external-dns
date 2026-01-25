1756cdd5d1e37af75c41192f982aa58a0a80591c
Ivan Ka <5395690+ivankatliarchuk@users.noreply.github.com>
2026-01-25 09:02:13 +0000

    chore(provider): zone cache provider interface (#6120)
    * chore(provider): zone cache provider interface
    
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>
    
    * chore(provider): zone cache provider interface
    
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>
    
    * chore(provider): zone cache provider interface
    
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>
    
    * chore(provider): zone cache provider interface
    
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>
    
    * chore(provider): zone cache provider interface
    
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>
    
    * chore(provider): zone cache provider interface
    
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>
    
    * chore(provider): zone cache provider interface
    
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>
    
    * chore(provider): zone cache provider interface
    
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>
    
    ---------
    
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- provider/aws/aws.go
+++ provider/aws/aws.go
@@ -32,6 +32,8 @@ import (
 	route53types "github.com/aws/aws-sdk-go-v2/service/route53/types"
 	log "github.com/sirupsen/logrus"
 
+	"sigs.k8s.io/external-dns/provider/blueprint"
+
 	"sigs.k8s.io/external-dns/endpoint"
 	"sigs.k8s.io/external-dns/plan"
 	"sigs.k8s.io/external-dns/provider"
@@ -282,12 +284,6 @@ func (z zoneTags) append(id string, tags []route53types.Tag) {
 	}
 }
 
-type zonesListCache struct {
-	age      time.Time
-	duration time.Duration
-	zones    map[string]*profiledZone
-}
-
 // AWSProvider is an implementation of Provider for AWS Route53.
 type AWSProvider struct {
 	provider.BaseProvider
@@ -309,7 +305,7 @@ type AWSProvider struct {
 	// extend filter for subdomains in the zone (e.g. first.us-east-1.example.com)
 	zoneMatchParent bool
 	preferCNAME     bool
-	zonesCache      *zonesListCache
+	zonesCache      *blueprint.ZoneCache[map[string]*profiledZone]
 	// queue for collecting changes to submit them in the next iteration, but after all other changes
 	failedChangesQueue map[string]Route53Changes
 }
@@ -347,7 +343,7 @@ func NewAWSProvider(awsConfig AWSConfig, clients map[string]Route53API) (*AWSPro
 		evaluateTargetHealth:  awsConfig.EvaluateTargetHealth,
 		preferCNAME:           awsConfig.PreferCNAME,
 		dryRun:                awsConfig.DryRun,
-		zonesCache:            &zonesListCache{duration: awsConfig.ZoneCacheDuration},
+		zonesCache:            blueprint.NewZoneCache[map[string]*profiledZone](awsConfig.ZoneCacheDuration),
 		failedChangesQueue:    make(map[string]Route53Changes),
 	}
 
@@ -370,11 +366,12 @@ func (p *AWSProvider) Zones(ctx context.Context) (map[string]*route53types.Hoste
 
 // zones returns the list of zones per AWS profile
 func (p *AWSProvider) zones(ctx context.Context) (map[string]*profiledZone, error) {
-	if p.zonesCache.zones != nil && time.Since(p.zonesCache.age) < p.zonesCache.duration {
-		log.Debug("Using cached zones list")
-		return p.zonesCache.zones, nil
+	if !p.zonesCache.Expired() {
+		cachedZones := p.zonesCache.Get()
+		log.Debugf("Using cached AWS zones, zone count: %d.", len(cachedZones))
+		return cachedZones, nil
 	}
-	log.Debug("Refreshing zones list cache")
+	log.Debug("Retrieving AWS zones.")
 
 	zones := make(map[string]*profiledZone)
 
@@ -437,11 +434,7 @@ func (p *AWSProvider) zones(ctx context.Context) (map[string]*profiledZone, erro
 		}
 	}
 
-	if p.zonesCache.duration > time.Duration(0) {
-		p.zonesCache.zones = zones
-		p.zonesCache.age = time.Now()
-	}
-
+	p.zonesCache.Reset(zones)
 	return zones, nil
 }
 
