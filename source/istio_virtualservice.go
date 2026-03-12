aa2a488b53af7d5651d7437018be203995e9d32b
Ivan Ka <5395690+ivankatliarchuk@users.noreply.github.com>
2026-03-12 07:27:36 +0000

    refactore(source): move EndpointsForHostname to endpoints package (#6248)
    * refactore(source): move EndpointsForHostname to endpoint package
    
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>
    
    * refactore(source): move EndpointsForHostname to endpoint package
    
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>
    
    * refactore(source): move EndpointsForHostname to endpoint package
    
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>
    
    ---------
    
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- source/istio_virtualservice.go
+++ source/istio_virtualservice.go
@@ -242,7 +242,7 @@ func (sc *virtualServiceSource) endpointsFromTemplate(ctx context.Context, virtu
 		if err != nil {
 			return endpoints, err
 		}
-		endpoints = append(endpoints, EndpointsForHostname(hostname, targets, ttl, providerSpecific, setIdentifier, resource)...)
+		endpoints = append(endpoints, endpoint.EndpointsForHostname(hostname, targets, ttl, providerSpecific, setIdentifier, resource)...)
 	}
 	return endpoints, nil
 }
@@ -314,7 +314,7 @@ func (sc *virtualServiceSource) endpointsFromVirtualService(ctx context.Context,
 			}
 		}
 
-		endpoints = append(endpoints, EndpointsForHostname(host, targets, ttl, providerSpecific, setIdentifier, resource)...)
+		endpoints = append(endpoints, endpoint.EndpointsForHostname(host, targets, ttl, providerSpecific, setIdentifier, resource)...)
 	}
 
 	// Skip endpoints if we do not want entries from annotations
@@ -328,7 +328,7 @@ func (sc *virtualServiceSource) endpointsFromVirtualService(ctx context.Context,
 					return endpoints, err
 				}
 			}
-			endpoints = append(endpoints, EndpointsForHostname(hostname, targets, ttl, providerSpecific, setIdentifier, resource)...)
+			endpoints = append(endpoints, endpoint.EndpointsForHostname(hostname, targets, ttl, providerSpecific, setIdentifier, resource)...)
 		}
 	}
 
