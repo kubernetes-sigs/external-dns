36d65d3359e9f68cee2a862bbe27306b68ac8550
Michel Loiseleur <michel.loiseleur@traefik.io>
2026-09-01 11:16:11 +0200

    chore(crd): sort recordType enum

--- endpoint/endpoint.go
+++ endpoint/endpoint.go
@@ -322,7 +322,7 @@ type Endpoint struct {
 	Targets Targets `json:"targets,omitempty"`
 	// RecordType is the DNS record type.
 	// +kubebuilder:validation:Required
-	// +kubebuilder:validation:Enum=A;AAAA;CNAME;TXT;SRV;NS;PTR;MX;NAPTR;DNAME
+	// +kubebuilder:validation:Enum=A;AAAA;CNAME;DNAME;MX;NAPTR;NS;PTR;SRV;TXT
 	RecordType string `json:"recordType,omitempty"`
 	// SetIdentifier distinguishes multiple records with the same name and type
 	// (e.g. Route53 records with routing policies other than 'simple').
